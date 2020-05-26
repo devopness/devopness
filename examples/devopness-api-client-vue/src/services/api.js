c9d4a717d6d46aaa69539a1cb308f365d317227c
Marcos Santos <marcosadriano034@gmail.com>
2020-05-26 12:54:18 -0300

    enh: update library SDK API

--- examples/devopness-api-client-vue/src/services/api.js
+++ examples/devopness-api-client-vue/src/services/api.js
@@ -1,5 +1,5 @@
-import { DevopnessApiClient } from 'devopness-sdk-js'
+import { DevopnessApiClient } from '@devopness/sdk-js'
 
 const DEVOPNESS_API_BASE_URL = 'https://test-api.devopness.com'
 
-export const devopnessApi = new DevopnessApiClient({ baseUrl: DEVOPNESS_API_BASE_URL })
+export const devopnessApi = new DevopnessApiClient({ baseURL: DEVOPNESS_API_BASE_URL })
