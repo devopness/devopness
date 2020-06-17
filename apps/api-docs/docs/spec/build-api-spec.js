d5977d4e058cb59e7e6008fb157fd1b9002b5215
brunoro <brunoro@users.noreply.github.com>
2020-06-17 16:03:29 -0300

    dredd user auth test against dev-api (#7)
    * rewrite hooks in modern js
    
    * uses beforeAll hook to sort and select transaction
    
    * users-signup runs against live api
    
    * `format: string` breaks dredd
    
    * dev-api endpoint should be https
    
    * users-refresh-token runs against live api
    
    * removes unused npm script
    
    * s/tx/transaction/

--- apps/api-docs/docs/spec/build-api-spec.js
+++ apps/api-docs/docs/spec/build-api-spec.js
@@ -1,29 +1,28 @@
-const DIR_SPEC = './spec';
-const DIR_BUILD = './build';
+const JsonRefs = require('json-refs');
+const YAML = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
 
-var JsonRefs = require('json-refs');
-var PathLoader = require('path-loader');
-var YAML = require('js-yaml');
-var fs = require('fs');
-var root = YAML.load(fs.readFileSync(DIR_SPEC + '/api-docs.yaml').toString());
-var options = {
-    loaderOptions: {
-        processContent: function(res, callback) {
-            callback(YAML.safeLoad(res.text));
-        }
-    },
-    location: DIR_SPEC + '/api-docs.yaml'
-};
-JsonRefs.resolveRefs(root, options).then(function(results) {
-    if (!fs.existsSync(DIR_BUILD)) {
-        fs.mkdirSync(DIR_BUILD);
+async function buildYamlSpec(srcPath, destPath) {
+    const srcYaml = YAML.load(fs.readFileSync(srcPath).toString());
+    const options = {
+        loaderOptions: {
+            processContent: function(res, callback) {
+                callback(YAML.safeLoad(res.text));
+            }
+        },
+        location: srcPath
+    };
+    const results = await JsonRefs.resolveRefs(srcYaml, options);
+
+    const destDir = path.dirname(destPath);
+    if (!fs.existsSync(destDir)) {
+        fs.mkdirSync(destDir);
     }
-    fs.writeFileSync(DIR_BUILD + '/api-docs-output.yaml', YAML.dump(results.resolved), function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Specification compiled successfully.');
-    });
-}, function(err) {
-    console.log(err);
-});
+    fs.writeFileSync(destPath, YAML.dump(results.resolved));
+    console.log(`Specification written at '${destPath}'`);
+}
+
+const src = './spec/api-docs.yaml';
+const dest = './build/api-docs-output.yaml';
+buildYamlSpec(src, dest);
--- apps/api-docs/dredd-hooks.js
+++ apps/api-docs/dredd-hooks.js
@@ -7,93 +7,121 @@
  * @todo: we can increment it by after sign-up and login create a project and a server inside a project,
  * assuming a default project environment for the server
  */
-var hooks = require('hooks');
-var testStash = {
-    acessToken: null,
-    refreshToken: null
-};
+const uuid = require('uuid');
+const hooks = require('hooks');
 
 // transaction names can be obtained by running `npx dredd --names`
 const transactionNames = {
+    'users-signup': 'Users > /users > Sign up/register a new user > 201',
     'users-login': 'Users > /users/login > Login/create a new token for the given credentials > 200 > application/json; charset=utf-8',
     'users-logout': 'Users > /users/logout > Logout/revoke an existing token > 204',
     'users-refresh-token': 'Users > /users/refresh-token > Refresh an existing user access token > 200 > application/json; charset=utf-8',
-    'users-signup': 'Users > /users/signup > Sign up/register a new user > 201 > application/json'
 };
 
-function updateTokens(transaction) {
-    if (transaction.results.statusCode &&
-        transaction.results.statusCode.valid &&
-        transaction.results.body.valid &&
-        transaction.results.body.valid) {
-        var responseData = JSON.parse(transaction.results.body.values.actual);
-        testStash.accessToken = responseData.access_token;
-        testStash.refreshToken = responseData.refresh_token;
-        // hooks.log("\nNew tokens retrieved:\n" + " accessToken: " + responseData.access_token);
-    }
-}
+// transactionOrder specifies which methods will run in which order
+const transactionOrder = [
+    'users-signup',
+    'users-login',
+    'users-refresh-token',
+    'users-logout'
+].map(k => transactionNames[k]);
 
-hooks.beforeEach(function(transaction) {
-    // hooks.log('Executing hook "beforeEach" for transaction "' + transaction.name + '"');
-    if(transaction.request.headers['Authorization'] != undefined) {
-        // transaction requires Authorization header, according to documentation
-        if (transaction.request.headers.Authorization == '') {
-            if (testStash.accessToken == null) {
-                // TO DO: should invoke request token, see git stash generated for tremtec
-                // in the beginning of the project
-                // updateTokens(transaction);
-            }
-            if (testStash.accessToken != null) {
-                transaction.request.headers.Authorization = 'Bearer ' + testStash.accessToken;
+const after = (id, cb) => hooks.after(transactionNames[id], cb);
+const before = (id, cb) => hooks.before(transactionNames[id], cb);
+
+// define which transactions will be ran and in which order
+// inspired by https://github.com/apiaryio/dredd/issues/456
+hooks.beforeAll((transactions, done) => {
+    let keep = [];
+    for (transaction of transactions) {
+        const idx = transactionOrder.indexOf(transaction.name);
+        if (idx > -1) {
+            keep[idx] = transaction;
+        }
+    }
+    transactions.splice(0, transactions.length);
+    for (i in keep) {
+        transactions.push(keep[i])
+    }
+    done();
+})
+
+const fixtures = {};
+const getFixture = (key) => fixtures[key];
+const putFixture = (key, value) => {
+    // hooks.log(`storing fixture "${key}": ${value}`);
+    fixtures[key] = value;
+};
+const deleteFixture = (key) => {
+    // hooks.log(`deleting fixture "${key}"`);
+    delete fixtures[key];
+};
+
+const storeFixtureFromTx = (key) => (transaction) => {
+    if (transaction.results.valid && transaction.real.body) {
+        const data = JSON.parse(transaction.real.body);
+        putFixture(key, data);
+    }
+};
+
+hooks.beforeEach((transaction) => {
+    // attach auth header if request requires it
+    if(transaction.request.headers.hasOwnProperty('Authorization')) {
+        if (transaction.request.headers.Authorization === '') {
+            const authToken = getFixture('authToken');
+            if (authToken && authToken.access_token) {
+                transaction.request.headers.Authorization = 'Bearer ' + authToken.access_token;
+            } else {
+                hooks.log(`'${transaction.id}' requires 'authToken' fixture; skipping`)
             }
         }
     }
     transaction.request.headers['Content-Type'] = 'application/json';
     transaction.request.headers.Accept = 'application/json';
-    // console.log(transaction.request);
 });
 
-// hooks.afterEach(function(transaction) {
-//     if (!transaction.results.statusCode.valid ||
-//         !transaction.results.headers.valid ||
-//         !(transaction.results.body != undefined && transaction.results.body.valid)) {
-//         hooks.log("Test failed. Full transaction results:\n");
-//         console.log(transaction.results);
-//     }
-// });
+///////////////////////////////////////////////////////////////////////////////
+// users
+///////////////////////////////////////////////////////////////////////////////
 
-hooks.before(transactionNames['users-login'], function(transaction) {
-    // hooks.log('Executing hook "before" transaction "' + transactionNames['users-login'] + '"');
+before('users-signup', (transaction) => {
+    const email = `${uuid.v1()}@api-test.devopness`;
+    const password = uuid.v4();
+    const credentials = { email, password } ;
 
-    var testCredentials = {
-        "email": "blabla@test.com",
-        "password": "algumasenha"
-    };
-    transaction.request.body = JSON.stringify(testCredentials);
+    transaction.request.body = JSON.stringify(credentials);
+    putFixture('userCredentials', credentials);
+})
 
-    // console.log(transaction.request);
+// after('users-signup', storeFixtureFromTx('user'));
+
+before('users-login', (transaction) => {
+    const user = getFixture('userCredentials');
+    if (user) {
+        const credentials = { email: user.email, password: user.password } ;
+        transaction.request.body = JSON.stringify(credentials);
+    } else {
+        hooks.log(`transaction '${transaction.id}' requires 'userCredentials' fixture; skipping`)
+        transaction.skip = true;
+    }
 });
+after('users-login', storeFixtureFromTx('authToken'));
 
-hooks.after(transactionNames['users-login'], function(transaction) {
-    updateTokens(transaction);
+before('users-refresh-token', (transaction) => {
+    const authToken = getFixture('authToken');
+    if (authToken && authToken.refresh_token) {
+        const body = JSON.parse(transaction.request.body);
+        body.refresh_token = authToken.refresh_token;
+        transaction.request.body = JSON.stringify(body);
+    } else {
+        hooks.log(`'${transaction.id}' requires 'authToken' fixture; skipping`)
+        transaction.skip = true;
+    }
 });
+after('users-refresh-token', storeFixtureFromTx('authToken'));
 
-hooks.before(transactionNames['users-logout'], function(transaction) {
-    // skipping the logout API call for now, to avoid revoking the existing token
-    // @todo: automatically detect endpoints that require authentication and call /login again?
-    transaction.skip = true;
-});
-
-hooks.before(transactionNames['users-refresh-token'], function(transaction) {
-    // hooks.log('Executing hook "before" transaction users-refresh-token');
-
-    var body = JSON.parse(transaction.request.body);
-    body.refresh_token = testStash.refreshToken;
-    transaction.request.body = JSON.stringify(body);
-
-    // console.log(transaction.request);
-});
-
-hooks.after(transactionNames['users-refresh-token'], function(transaction) {
-    updateTokens(transaction);
+after('users-logout', (transaction) => {
+    if (transaction.results.valid) {
+        deleteFixture('authToken');
+    }
 });
