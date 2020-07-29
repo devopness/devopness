0616ef1091ba4cb57d905f5e145766d5f220693a
Jeferson Oliveira <jefersonfoliveira@gmail.com>
2020-07-29 00:18:26 -0300

    fix: export ApiResponse class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './DevopnessApiClient';
+export * from './common/ApiResponse';
 export * as SdkExceptions from './common/Exceptions';
 export * as SdkModels from './api/generated/models';
