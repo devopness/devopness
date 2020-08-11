e9c3c2b928284e70f850a09cd9e6c711cd059dae
Jeferson Oliveira <jefersonfoliveira@gmail.com>
2020-08-11 16:28:31 -0300

    fix: expose app variables methods

--- src/services/ApplicationService.ts
+++ src/services/ApplicationService.ts
@@ -3,16 +3,20 @@ import { ApplicationsApiService } from '../api/generated/apis/applications-api';
 import { ApplicationsDeploymentsApiService } from '../api/generated/apis/applications-deployments-api';
 import { ApplicationsSSLCertificatesApiService } from '../api/generated/apis/applications-sslcertificates-api';
 import { mergeSiblingClasses } from '../common/MergeSiblingClasses';
+import { ApplicationsVariablesApiService } from '../api/generated/apis/applications-variables-api';
 
-export class ApplicationService extends ApiBaseService {}
+export class ApplicationService extends ApiBaseService { }
 export interface ApplicationService extends
     ApplicationsApiService,
     ApplicationsDeploymentsApiService,
     ApplicationsApiService,
-    ApplicationsSSLCertificatesApiService {}
+    ApplicationsSSLCertificatesApiService,
+    ApplicationsVariablesApiService {
+}
 
 mergeSiblingClasses(ApplicationService, [
     ApplicationsApiService,
     ApplicationsDeploymentsApiService,
-    ApplicationsSSLCertificatesApiService
+    ApplicationsSSLCertificatesApiService,
+    ApplicationsVariablesApiService
 ]);
