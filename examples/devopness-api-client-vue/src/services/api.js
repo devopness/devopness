b7b880d45815f3339aea807bdaa6d33ef99723a0
Luiz Felipe <felipe.silva337@yahoo.com>
2020-09-08 16:11:59 -0300

    feat: delete 'examples' folder

--- examples/devopness-api-client-vue/src/services/api.js
+++
@@ -1,5 +0,0 @@
-import { DevopnessApiClient } from '@devopness/sdk-js'
-
-const DEVOPNESS_API_BASE_URL = 'https://dev-api.devopness.com'
-
-export const devopnessApi = new DevopnessApiClient({ baseURL: DEVOPNESS_API_BASE_URL })
