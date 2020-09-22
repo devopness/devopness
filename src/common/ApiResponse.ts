1f92268a711e7d0580e632bf18a072e6cce99842
Luiz Felipe <felipe.silva337@yahoo.com>
2020-09-22 16:34:38 -0300

    feat: update ApiResponse to add pageCount attribute

--- src/common/ApiResponse.ts
+++ src/common/ApiResponse.ts
@@ -1,11 +1,22 @@
 import { AxiosResponse } from 'axios'
+import parseLinkHeader from 'parse-link-header'
+
+interface ApiLinks {
+    last: {
+        page: number;
+    }
+}
 
 export class ApiResponse<T>{
-    status: number;
     data: T;
+    pageCount?: number;
+    status: number;
 
     constructor(axiosResp: AxiosResponse<T>) {
         this.status = axiosResp.status;
         this.data = axiosResp.data;
+
+        const linkHeader = parseLinkHeader(axiosResp.headers?.link) as ApiLinks | null;
+        this.pageCount = Number(linkHeader?.last.page) || undefined;
     }
 }
