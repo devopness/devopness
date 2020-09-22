8aff7574be4d345e99218bdd3ca977c08f60c397
Luiz Felipe <felipe.silva337@yahoo.com>
2020-09-22 16:47:42 -0300

    test: add test to check if ApiResponse don't break if no have Link header

--- tests/ApiResponse.spec.ts
+++ tests/ApiResponse.spec.ts
@@ -17,3 +17,29 @@ test('ApiResponse has the correct pageCount value', () => {
 
     expect(response.pageCount).toBe(expectedPage);
 });
+
+test('ApiResponse do not fail if no have Link header', () => {
+    const axiosResponse = {
+        data: null,
+        status: 200,
+        headers: {},
+    } as AxiosResponse<null>;
+
+    const response = new ApiResponse<null>(axiosResponse);
+
+    expect(response.pageCount).toBe(undefined);
+});
+
+test('If no have last link, expect undefined pageCount', () => {
+    const axiosResponse = {
+        data: null,
+        status: 200,
+        headers: {
+            link: `<https://test.com/path?page=1>; rel="first"`,
+        },
+    } as AxiosResponse<null>;
+
+    const response = new ApiResponse<null>(axiosResponse);
+
+    expect(response.pageCount).toBe(undefined);
+});
