4af913cdbf9f4ff094428ea0f2738bd9e4d6778a
Luiz Felipe <felipe.silva337@yahoo.com>
2020-09-22 17:26:42 -0300

    test: update test to be more clearly

--- tests/ApiResponse.spec.ts
+++ tests/ApiResponse.spec.ts
@@ -3,22 +3,22 @@ import { ApiResponse } from '../src/common/ApiResponse';
 
 test('ApiResponse has the correct pageCount value', () => {
     const baseUrl = 'https://test.com/path';
-    const expectedPage = 7;
+    const expectedPageCount = 7;
 
     const axiosResponse = {
         data: null,
         status: 200,
         headers: {
-            link: `<${baseUrl}?page=1>; rel="first", <${baseUrl}?page=${expectedPage}>; rel="last"`,
+            link: `<${baseUrl}?page=1>; rel="first", <${baseUrl}?page=${expectedPageCount}>; rel="last"`,
         },
     } as AxiosResponse<null>;
 
     const response = new ApiResponse<null>(axiosResponse);
 
-    expect(response.pageCount).toBe(expectedPage);
+    expect(response.pageCount).toBe(expectedPageCount);
 });
 
-test('ApiResponse do not fail if no have Link header', () => {
+test('ApiResponse do not fail if no Link header present', () => {
     const axiosResponse = {
         data: null,
         status: 200,
