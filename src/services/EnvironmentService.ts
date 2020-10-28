81944ae4e6f1ce369f7bb43491e2b6bb0d0dc380
Luiz Felipe <felipe.silva337@yahoo.com>
2020-10-28 17:57:08 -0300

    feat: add `applications` to EnvironmentService

--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -1,8 +1,10 @@
 import { EnvironmentsApiService } from '../api/generated/apis/environments-api';
+import { EnvironmentsApplicationsApiService } from '../api/generated/apis/environments-applications-api';
 import { EnvironmentsCronJobsApiService } from '../api/generated/apis/environments-cron-jobs-api';
 import { EnvironmentsServersApiService } from '../api/generated/apis/environments-servers-api';
 
 export class EnvironmentService extends EnvironmentsApiService {
+    public applications = new EnvironmentsApplicationsApiService();
     public cronjobs = new EnvironmentsCronJobsApiService();
     public servers = new EnvironmentsServersApiService();
 }
