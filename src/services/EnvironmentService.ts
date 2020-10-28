e5936d1cd0aafeab86f23dbe412a70bdf7dfdba1
Luiz Felipe <felipe.silva337@yahoo.com>
2020-10-28 14:40:26 -0300

    feat: add daemons to EnvironmentService

--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -1,8 +1,10 @@
 import { EnvironmentsApiService } from '../api/generated/apis/environments-api';
 import { EnvironmentsCronJobsApiService } from '../api/generated/apis/environments-cron-jobs-api';
+import { EnvironmentsDaemonsApiService } from '../api/generated/apis/environments-daemons-api';
 import { EnvironmentsServersApiService } from '../api/generated/apis/environments-servers-api';
 
 export class EnvironmentService extends EnvironmentsApiService {
     public cronjobs = new EnvironmentsCronJobsApiService();
+    public daemons = new EnvironmentsDaemonsApiService();
     public servers = new EnvironmentsServersApiService();
 }
