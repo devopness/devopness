9d5073c368a10fa62b88b56a4084ce6da0d93740
Luiz Felipe <felipe.silva337@yahoo.com>
2020-10-28 12:04:01 -0300

    feat: expose cronjobs on EnvironmentService

--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -1,6 +1,8 @@
 import { EnvironmentsApiService } from '../api/generated/apis/environments-api';
+import { EnvironmentsCronJobsApiService } from '../api/generated/apis/environments-cron-jobs-api';
 import { EnvironmentsServersApiService } from '../api/generated/apis/environments-servers-api';
 
 export class EnvironmentService extends EnvironmentsApiService {
+    public cronjobs = new EnvironmentsCronJobsApiService();
     public servers = new EnvironmentsServersApiService();
 }
