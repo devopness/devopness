3465b64f658196e7300edbb6b9a2ac1966d334ee
Luiz Felipe <felipe.silva337@yahoo.com>
2020-10-29 08:26:23 -0300

    feat: add 'networkRules' to EnvironmentService

--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -2,11 +2,13 @@ import { EnvironmentsApiService } from '../api/generated/apis/environments-api';
 import { EnvironmentsApplicationsApiService } from '../api/generated/apis/environments-applications-api';
 import { EnvironmentsCronJobsApiService } from '../api/generated/apis/environments-cron-jobs-api';
 import { EnvironmentsDaemonsApiService } from '../api/generated/apis/environments-daemons-api';
+import { EnvironmentsNetworkRulesApiService } from '../api/generated/apis/environments-network-rules-api';
 import { EnvironmentsServersApiService } from '../api/generated/apis/environments-servers-api';
 
 export class EnvironmentService extends EnvironmentsApiService {
     public applications = new EnvironmentsApplicationsApiService();
     public cronjobs = new EnvironmentsCronJobsApiService();
     public daemons = new EnvironmentsDaemonsApiService();
+    public networkRules = new EnvironmentsNetworkRulesApiService();
     public servers = new EnvironmentsServersApiService();
 }
