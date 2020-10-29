f171bd758d32e70ed13774ef33acf52c2b04a6b1
Luiz Felipe <felipe.silva337@yahoo.com>
2020-10-29 08:56:36 -0300

    feat: add 'sshKeys' to EnvironmentService

--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -3,10 +3,12 @@ import { EnvironmentsApplicationsApiService } from '../api/generated/apis/enviro
 import { EnvironmentsCronJobsApiService } from '../api/generated/apis/environments-cron-jobs-api';
 import { EnvironmentsDaemonsApiService } from '../api/generated/apis/environments-daemons-api';
 import { EnvironmentsServersApiService } from '../api/generated/apis/environments-servers-api';
+import { EnvironmentsSSHKeysApiService } from '../api/generated/apis/environments-sshkeys-api';
 
 export class EnvironmentService extends EnvironmentsApiService {
     public applications = new EnvironmentsApplicationsApiService();
     public cronjobs = new EnvironmentsCronJobsApiService();
     public daemons = new EnvironmentsDaemonsApiService();
     public servers = new EnvironmentsServersApiService();
+    public sshKeys = new EnvironmentsSSHKeysApiService();
 }
