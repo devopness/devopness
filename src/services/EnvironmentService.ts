7d1ddcb9f5eef8ed7f26426101a9f5fe8dc3c58d
Luiz Felipe <felipe.silva337@yahoo.com>
2020-10-29 07:33:19 -0300

    feat: add 'services' attribute to EnvironmentService

--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -3,10 +3,12 @@ import { EnvironmentsApplicationsApiService } from '../api/generated/apis/enviro
 import { EnvironmentsCronJobsApiService } from '../api/generated/apis/environments-cron-jobs-api';
 import { EnvironmentsDaemonsApiService } from '../api/generated/apis/environments-daemons-api';
 import { EnvironmentsServersApiService } from '../api/generated/apis/environments-servers-api';
+import { EnvironmentsServicesApiService } from '../api/generated/apis/environments-services-api';
 
 export class EnvironmentService extends EnvironmentsApiService {
     public applications = new EnvironmentsApplicationsApiService();
     public cronjobs = new EnvironmentsCronJobsApiService();
     public daemons = new EnvironmentsDaemonsApiService();
     public servers = new EnvironmentsServersApiService();
+    public services = new EnvironmentsServicesApiService();
 }
