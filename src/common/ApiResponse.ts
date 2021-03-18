5fb6118598a267f955b5d8d43b1a919550d0a47a
Luiz Felipe <felipe.silva337@yahoo.com>
2021-03-18 11:42:46 -0300

    fix: set 'pageCount' to 1 if 'Link' header is not defined

--- src/common/ApiResponse.ts
+++ src/common/ApiResponse.ts
@@ -10,16 +10,17 @@ interface ApiLinks {
 export class ApiResponse<T>{
     actionId?: number;
     data: T;
-    pageCount?: number;
+    pageCount: number;
     status: number;
 
     constructor(axiosResp: AxiosResponse<T>) {
         this.status = axiosResp.status;
         this.data = axiosResp.data;
+        this.pageCount = 1;
 
         if (axiosResp.headers) {
-            const linkHeader = parseLinkHeader(axiosResp.headers?.link) as ApiLinks | null;
-            this.pageCount = Number(linkHeader?.last?.page) || undefined;
+            const linkHeader = parseLinkHeader(axiosResp.headers.link) as ApiLinks | null;
+            this.pageCount = Number(linkHeader?.last?.page) || 1;
             // axios headers are intentionally lower cased, as per https://github.com/axios/axios/issues/413
             this.actionId = axiosResp.headers['x-devopness-action-id'];
         }
