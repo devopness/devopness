5b795c2ca26058e028ae58677925a48eb796d75d
Luiz Felipe <felipe.silva337@yahoo.com>
2021-05-28 14:31:49 -0300

    feat: update TeamService

--- src/services/TeamService.ts
+++ src/services/TeamService.ts
@@ -1,4 +1,8 @@
 import { TeamsApiService } from '../api/generated/apis/teams-api';
+import { TeamsInvitationsApiService } from '../api/generated/apis/teams-invitations-api';
+import { TeamsMembersApiService } from '../api/generated/apis/teams-members-api';
 
 export class TeamService extends TeamsApiService {
+    public invitations = new TeamsInvitationsApiService();
+    public members = new TeamsMembersApiService();
 }
