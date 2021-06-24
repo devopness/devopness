6f3bb08f09a97024cfa214bb2e4ce8fb2a9c82fe
Luiz Felipe <felipe.silva337@yahoo.com>
2021-06-24 15:10:58 -0300

    test: add tests to ServerService

---
+++ tests/ServerService.spec.ts
@@ -0,0 +1,18 @@
+import { DevopnessApiClient } from '../src/DevopnessApiClient';
+
+const api = new DevopnessApiClient();
+
+test('convertToHostname() util method', () => {
+    const list = [
+        ['Maria da Conceição', 'maria-da-conceicao'],
+        ['-2+5=7? não véi, cê é doido? e o signed two?', '2-5-7-nao-vei-ce-e-doido-e-o-signed-two'],
+        [' Espaço no início melhor ignorar---varioshifens_tb', 'espaco-no-inicio-melhor-ignorar-varioshifens-tb'],
+        ['-- test --', 'test'],
+        ['long     space', 'long-space'],
+    ];
+
+    for (let [input, expectedOutput] of list) {
+        const output = api.servers.convertToHostname(input);
+        expect(output).toBe(expectedOutput);
+    }
+});
