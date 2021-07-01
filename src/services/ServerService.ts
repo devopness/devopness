291dc3722d4a43addc8316a0d357e8173bc21090
Luiz Felipe <felipe.silva337@yahoo.com>
2021-07-01 18:52:22 -0300

    feat: add utility method to get valid hostname (#326)
    * feat: implement convertToHostname() util method
    
    * test: add tests to ServerService
    
    * fix: remove useless escape and add return type
    
    * test: improve test to ServerService

--- src/services/ServerService.ts
+++ src/services/ServerService.ts
@@ -1,4 +1,17 @@
 import { ServersApiService } from '../api/generated/apis/servers-api';
 
 export class ServerService extends ServersApiService {
+    /**
+     * Converts a string to a valid server's hostname format
+     *
+     * @param name The given string to converts to hostname
+     */
+    convertToHostname(name: string): string {
+        return name
+            .toLowerCase()
+            .normalize('NFD')
+            .replace(/[\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E]+/g, '-')
+            .replace(/[^a-z0-9-]+|^[-\s]+|[-\s]+$/g, '')
+            .replace(/-{2,}/g, '-');
+    }
 }
