8a7cc1d7f916f687a8c431bb46508154721a0d09
Luísa Braga <mluisapbraga@gmail.com>
2022-07-27 14:07:47 -0400

    feat: static cloud provider service route

--- packages/sdks/javascript/src/api/generated/apis/static-data-api.ts
+++ packages/sdks/javascript/src/api/generated/apis/static-data-api.ts
@@ -15,7 +15,9 @@ import { ApiBaseService } from "../../../services/ApiBaseService";
 import { ApiResponse } from "../../../common/ApiResponse";
 import { ArgumentNullException } from "../../../common/Exceptions";
 import { ApplicationOptions } from '../../generated/models';
+import { CloudInstance } from '../../generated/models';
 import { CloudProvider } from '../../generated/models';
+import { CloudProviderService } from '../../generated/models';
 import { CronJobOptions } from '../../generated/models';
 import { EnvironmentOptions } from '../../generated/models';
 import { ServerOptions } from '../../generated/models';
@@ -27,6 +29,24 @@ import { ServiceOptions } from '../../generated/models';
 export class StaticDataApiService extends ApiBaseService {
     /**
      * 
+     * @summary Get details of a single cloud provider service
+     * @param {string} serviceCode The unique id of the service
+     */
+    public async getCloudProviderService(serviceCode: string): Promise<ApiResponse<CloudProviderService>> {
+        if (serviceCode === null || serviceCode === undefined) {
+            throw new ArgumentNullException('serviceCode', 'getCloudProviderService');
+        }
+        
+        let queryString = '';
+
+        const requestUrl = '/static/cloud-provider-services/{service_code}' + (queryString? `?${queryString}` : '');
+
+        const response = await this.get <CloudProviderService>(requestUrl.replace(`{${"service_code"}}`, encodeURIComponent(String(serviceCode))));
+        return new ApiResponse(response);
+    }
+
+    /**
+     * 
      * @summary Lists options of the applications
      */
     public async listApplicationOptions(): Promise<ApiResponse<ApplicationOptions>> {
@@ -36,6 +56,28 @@ export class StaticDataApiService extends ApiBaseService {
         const requestUrl = '/static/applications' + (queryString? `?${queryString}` : '');
 
         const response = await this.get <ApplicationOptions>(requestUrl);
+        return new ApiResponse(response);
+    }
+
+    /**
+     * 
+     * @summary Lists cloud provider service regions
+     * @param {string} serviceCode The unique id of the service
+     * @param {string} regionCode The unique id of the region
+     */
+    public async listCloudProviderServiceRegions(serviceCode: string, regionCode: string): Promise<ApiResponse<Array<CloudInstance>>> {
+        if (serviceCode === null || serviceCode === undefined) {
+            throw new ArgumentNullException('serviceCode', 'listCloudProviderServiceRegions');
+        }
+        if (regionCode === null || regionCode === undefined) {
+            throw new ArgumentNullException('regionCode', 'listCloudProviderServiceRegions');
+        }
+        
+        let queryString = '';
+
+        const requestUrl = '/static/cloud-provider-services/{service_code}/regions/{region_code}' + (queryString? `?${queryString}` : '');
+
+        const response = await this.get <Array<CloudInstance>>(requestUrl.replace(`{${"service_code"}}`, encodeURIComponent(String(serviceCode))).replace(`{${"region_code"}}`, encodeURIComponent(String(regionCode))));
         return new ApiResponse(response);
     }
 
