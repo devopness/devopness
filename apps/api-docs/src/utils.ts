c45d392fd04bc8c572bb3419f9e1ac367535f7ca
zilly <imnotzilly@gmail.com>
2022-08-03 16:03:29 -0300

    fix: replace deprecated string function in utils (#268)
    * fix: replace deprecated string function in utils
    
    * build: bump typescript to 4.9.7 and add ts-jest
    
    * test: add config file for jest
    
    * test: add utils function test
    
    * test: jest should be called alongside dredd
    
    * test: change test calls and order
    
    jest now has a dedicated commands on scripts and is called before dredd.
    
    * test: replace util function test
    
    Test now focuses on the util function instead of the String functions.
    
    * style: add final new line on jest config file
    
    * style: add VSCode project settings

--- apps/api-docs/src/utils.ts
+++ apps/api-docs/src/utils.ts
@@ -4,5 +4,5 @@
  */
 export function camelCaseToUnderscore(camelCase: string): string {
     const under = camelCase.replace(/([A-Z])/g, (g: string) => `_${g[0].toLowerCase()}`);
-    return under[0] == '_' ? under.substr(1) : under;
+    return under[0] == '_' ? under.substring(1) : under;
 }
