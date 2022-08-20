4cdf611d5388c586326ea3bb7eb6fd2a4f4a515b
zilly <imnotzilly@gmail.com>
2022-08-20 18:19:11 -0300

    feat: add pipeline steps (#570)
    * feat: add updated spec
    
    * feat: add script runners to static applications
    
    * feat: add pipelines steps
    
    * feat: add steps to pipeline service
    
    * feat: add runner name param to dedicated file
    
    * fix: rename summary and operationId in pipeline unlink

--- packages/sdks/javascript/src/services/PipelineService.ts
+++ packages/sdks/javascript/src/services/PipelineService.ts
@@ -1,6 +1,8 @@
 import { PipelinesApiService } from "../api/generated/apis/pipelines-api";
 import { PipelinesResourcesApiService } from "../api/generated/apis/pipelines-resources-api";
+import { PipelinesStepsApiService } from "../api/generated/apis/pipelines-steps-api";
 
 export class PipelineService extends PipelinesApiService {
     public resources = new PipelinesResourcesApiService();
+    public steps = new PipelinesStepsApiService();
 }
