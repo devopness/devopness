9d8cdabcae2be68a9425e8bbdcd98430c1e6f7fe
notzilly <imnotzilly@gmail.com>
2022-09-29 01:15:45 -0300

    feat: update user service to contain added endpoint

--- packages/sdks/javascript/src/services/UserService.ts
+++ packages/sdks/javascript/src/services/UserService.ts
@@ -1,4 +1,6 @@
 import { UsersApiService } from '../api/generated/apis/users-api';
+import { UsersTeamsApiService } from '../api/generated/apis/users-teams-api';
 
 export class UserService extends UsersApiService {
+    public teams = new UsersTeamsApiService();
 }
