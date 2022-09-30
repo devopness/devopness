15da50a5ad08e8208d7c528689b1c78c395298ae
zilly <imnotzilly@gmail.com>
2022-09-29 22:57:41 -0300

    feat: create list pending team invitations for authenticated user endpoint (#582)
    * feat: add list pending invitations  for authenticated user endpoint
    
    * feat: update user service to contain added endpoint
    
    * fix: update service name and replace model

--- packages/sdks/javascript/src/services/UserService.ts
+++ packages/sdks/javascript/src/services/UserService.ts
@@ -1,4 +1,6 @@
 import { UsersApiService } from '../api/generated/apis/users-api';
+import { UsersTeamInvitationsApiService } from '../api/generated/apis/users-team-invitations-api';
 
 export class UserService extends UsersApiService {
+    public teamInvitations = new UsersTeamInvitationsApiService();
 }
