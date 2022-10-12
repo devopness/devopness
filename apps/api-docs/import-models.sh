b5a740372d58f4b69c39e49f9e452c57f642c5ca
Luiz Felipe <felipe.silva337@yahoo.com>
2022-10-12 20:29:42 -0300

    feat: add workflow to create PR to import auto generated models (#289)
    * feat: add import-models.sh script
    
    * feat: add workflow to import auto generated models
    
    * feat: update workflow to create PR as devopness-automations user
    
    Co-authored-by: jfoliveira <jefersonfoliveira@gmail.com>

---
+++ apps/api-docs/import-models.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+BRANCH_NAME="feat/update-auto-generated-models"
+
+git config user.name devopness-automations
+git config user.email automations@devopness.com
+
+git fetch
+if git checkout "$BRANCH_NAME" 2> /dev/null; then
+    echo ::set-output name=pr_exists::true
+else
+    git checkout -b "$BRANCH_NAME"
+    echo ::set-output name=pr_exists::false
+fi
+
+npm run api-docs-zip-download
+npm run api-docs-zip-extract
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Auto generated models is up to date. Nothing to commit!"
+    echo ::set-output name=has_changes::false
+    exit 0
+fi
+
+git add .
+git commit -m "feat: update auto generated models"
+
+git push origin "$BRANCH_NAME"
+echo ::set-output name=has_changes::true
