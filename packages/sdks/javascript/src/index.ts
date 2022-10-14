239e425a7da887c33e092fe84d328985455ef218
Jeferson Oliveira <jefersonfoliveira@gmail.com>
2022-10-13 21:58:18 -0300

    feat: Export SdkServices types

--- packages/sdks/javascript/src/index.ts
+++ packages/sdks/javascript/src/index.ts
@@ -1,4 +1,6 @@
 export * from './DevopnessApiClient';
 export * from './common/ApiResponse';
+
 export * as SdkExceptions from './common/Exceptions';
 export * as SdkModels from './api/generated/models';
+export * as SdkServices from './services';
