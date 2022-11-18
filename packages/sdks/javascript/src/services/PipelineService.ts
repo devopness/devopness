dee4a30aa683ff41a23171c13e10359a9d698a3d
Luiz Felipe <felipe.silva337@yahoo.com>
2022-11-18 18:18:59 -0300

    feat: update PipelineService

--- packages/sdks/javascript/src/services/PipelineService.ts
+++ packages/sdks/javascript/src/services/PipelineService.ts
@@ -1,8 +1,6 @@
 import { PipelinesApiService } from "../api/generated/apis/pipelines-api";
-import { PipelinesResourcesApiService } from "../api/generated/apis/pipelines-resources-api";
 import { PipelinesStepsApiService } from "../api/generated/apis/pipelines-steps-api";
 
 export class PipelineService extends PipelinesApiService {
-    public resources = new PipelinesResourcesApiService();
     public steps = new PipelinesStepsApiService();
 }
