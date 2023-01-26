0ff3034ac2d28d441f22237376c9cd41ea80f33d
Devopness Automations <114699329+devopness-automations@users.noreply.github.com>
2023-01-26 17:02:31 -0300

    feat: 2023-01-25 17:33 - Update auto generated models (#345)
    * feat: update auto generated models
    
    * fix: correct trigger test name on skip list
    
    Co-authored-by: notzilly <imnotzilly@gmail.com>

--- apps/api-docs/src/hooks.ts
+++ apps/api-docs/src/hooks.ts
@@ -31,7 +31,7 @@ hooks.beforeAll((transactions: Transaction[], done: () => void) => {
         // A hook request is not created by the tests, so we don't have a valid hook_request_id
         'getHookRequest200',
         // @todo: Resolves conflicts to trigger a hook on dev-api
-        'triggerHook202',
+        'triggerHook200',
         'acceptInvitation204',
         // We do not create the member when the team is created, therefore we cannot retrieve a team member by its ID.
         'getTeamMember200',
