062f515fafb919ed28468ec42605d3e888380cd0
Luiz Felipe <felipe.silva337@yahoo.com>
2023-06-08 18:39:08 -0300

    feat: add `archivedEnvironments` to ProjectService

--- packages/sdks/javascript/src/services/ProjectService.ts
+++ packages/sdks/javascript/src/services/ProjectService.ts
@@ -3,9 +3,11 @@ import { ProjectsActionsApiService } from '../api/generated/apis/projects-action
 import { ProjectsEnvironmentsApiService } from '../api/generated/apis/projects-environments-api';
 import { ProjectsRolesApiService } from '../api/generated/apis/projects-roles-api';
 import { ProjectsTeamsApiService } from '../api/generated/apis/projects-teams-api';
+import { ProjectsArchivedEnvironmentsApiService } from '../api/generated/apis/projects-archived-environments-api';
 
 export class ProjectService extends ProjectsApiService {
   public actions = new ProjectsActionsApiService();
+  public archivedEnvironments = new ProjectsArchivedEnvironmentsApiService();
   public environments = new ProjectsEnvironmentsApiService();
   public roles = new ProjectsRolesApiService();
   public teams = new ProjectsTeamsApiService();
