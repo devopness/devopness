1e8e34462d76ed01c6878af157a51fcc53eaf8ba
Gabriel Viana <viana_gab2001@hotmail.com>
2023-06-29 07:50:44 -0300

    feat: add 'actions' to 'PipelineService' (#711)

--- packages/sdks/javascript/src/services/PipelineService.ts
+++ packages/sdks/javascript/src/services/PipelineService.ts
@@ -1,6 +1,8 @@
+import { PipelinesActionsApiService } from "../api/generated/apis/pipelines-actions-api";
 import { PipelinesApiService } from "../api/generated/apis/pipelines-api";
 import { PipelinesStepsApiService } from "../api/generated/apis/pipelines-steps-api";
 
 export class PipelineService extends PipelinesApiService {
+    public actions = new PipelinesActionsApiService()
     public steps = new PipelinesStepsApiService();
 }
