5f698810652c08c99f9ea4544300cf6776506d1f
Luiz Felipe <felipe.silva337@yahoo.com>
2023-08-29 22:28:30 -0300

    feat: add SubnetService

--- packages/sdks/javascript/src/services/NetworkService.ts
+++ packages/sdks/javascript/src/services/NetworkService.ts
@@ -1,4 +1,6 @@
 import { NetworksApiService } from "../api/generated/apis/networks-api";
+import { NetworksSubnetsApiService } from "../api/generated/apis/networks-subnets-api";
 
 export class NetworkService extends NetworksApiService {
+    public subnets = new NetworksSubnetsApiService();
 }
