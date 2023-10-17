ea54d492e285c1fc1dafe1cd45a968eeff6ccae0
Breno Lipi <43282619+brlipi@users.noreply.github.com>
2023-10-17 11:56:34 -0300

    feat: add UsersEnvironmentsApiService to UserService

--- packages/sdks/javascript/src/services/UserService.ts
+++ packages/sdks/javascript/src/services/UserService.ts
@@ -1,8 +1,10 @@
 import { UsersApiService } from '../api/generated/apis/users-api';
 import { UsersTeamInvitationsApiService } from '../api/generated/apis/users-team-invitations-api';
 import { UsersPasswordsApiService } from '../api/generated/apis/users-passwords-api';
+import { UsersEnvironmentsApiService } from '../api/generated/apis/users-environments-api';
 
 export class UserService extends UsersApiService {
     public teamInvitations = new UsersTeamInvitationsApiService();
     public passwords = new UsersPasswordsApiService();
+    public environments = new UsersEnvironmentsApiService();
 }
