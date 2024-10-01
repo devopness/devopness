62f8132e5e924313ef68dee38d48e77aa09bdaaf
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-01 19:53:13 -0300

    feaat: update eslint rules

--- packages/ui/react/eslint.config.js
+++ packages/ui/react/eslint.config.js
@@ -44,6 +44,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
