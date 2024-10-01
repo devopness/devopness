6a6d76c4356d0c94df0a142afd83e9ab99867dac
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-01 19:51:46 -0300

    fix: Object is not function

--- packages/ui/react/.storybook/devopness.ts
+++ packages/ui/react/.storybook/devopness.ts
@@ -1,10 +1,8 @@
 import { create } from '@storybook/theming/create'
 
-import { getImageAssetUrl } from 'src/icons'
-
 export default create({
   brandTitle: 'Devopness Design System React Components',
   brandUrl: 'https://www.devopness.com/',
-  brandImage: getImageAssetUrl('logo_devopness_alfa.png'),
+  brandImage: 'https://assets.devopness.com/images/logo_devopness_alfa.png',
   base: 'light',
 })
