7f0877e4b27aa30079bf855d9289aaaf8deed8e6
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-01 08:16:27 -0300

    feat: remove unused storybook addons

--- packages/ui/react/.storybook/main.ts
+++ packages/ui/react/.storybook/main.ts
@@ -5,10 +5,7 @@ const config: StorybookConfig = {
     '../src/**/*.stories.@(ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
