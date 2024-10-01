63736f875004329d52f06c45b05bd55b2435292c
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-01 06:37:43 -0300

    feat: replace babel with swc for faster fast refresh

--- packages/ui/react/vite.config.ts
+++ packages/ui/react/vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from "path"
 import { defineConfig } from 'vite'
 import dts from "vite-plugin-dts"
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
