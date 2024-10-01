b1dc29b11759092d817b734b24b449ac95eb52dd
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-01 07:11:49 -0300

    fix: eslint errors

--- packages/ui/react/src/stories/Page.tsx
+++ packages/ui/react/src/stories/Page.tsx
@@ -14,9 +14,9 @@ export const Page: React.FC = () => {
     <article>
       <Header
         user={user}
-        onLogin={() => setUser({ name: 'Jane Doe' })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
+        onLogin={() => { setUser({ name: 'Jane Doe' }); }}
+        onLogout={() => { setUser(undefined); }}
+        onCreateAccount={() => { setUser({ name: 'Jane Doe' }); }}
       />
 
       <section className="storybook-page">
@@ -36,7 +36,7 @@ export const Page: React.FC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose such data from the
-            "args" of child component stories
+            &quot;args&quot; of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock these services out
