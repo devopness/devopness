c14964c944eaddebfd0777473e42dd302299d68a
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-02 10:43:28 -0300

    feat: replace hardcoded url with dynamic method

--- packages/ui/react/.storybook/devopness.ts
+++ packages/ui/react/.storybook/devopness.ts
@@ -1,8 +1,10 @@
 import { create } from '@storybook/theming/create'
 
+import { getImageAssetUrl } from '../src/icons/getImageAssetUrl'
+
 export default create({
   brandTitle: 'Devopness Design System React Components',
   brandUrl: 'https://www.devopness.com/',
-  brandImage: 'https://assets.devopness.com/images/logo_devopness_alfa.png',
+  brandImage: getImageAssetUrl('logo_devopness_alfa.png'),
   base: 'light',
 })
