439c921e40899e9e54f631458fdd1f7e9e429260
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-02 11:01:41 -0300

    feat: add Devopness Button component

--- packages/ui/react/src/components/Buttons/Button/Button.stories.ts
+++ packages/ui/react/src/components/Buttons/Button/Button.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { fn } from '@storybook/test'
 
 import { Button } from './Button'
+import { iconList } from 'src/icons/iconLoader'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -16,7 +17,12 @@ const meta = {
   ],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    icon: {
+      control: 'select',
+      options: iconList.map(({ accessor }) => accessor),
+    },
+    iconColor: { control: 'color' },
+    children: { control: 'text' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -27,30 +33,12 @@ type Story = StoryObj<typeof meta>
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-}
-
-const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-}
-
-const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-}
-
-const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    icon: 'plus',
+    children: 'Add Application',
+    typeSize: 'default',
+    buttonType: 'Default'
   },
 }
 
 export default meta
-export { Primary, Secondary, Large, Small }
+export { Primary }
--- packages/ui/react/src/components/Buttons/Button/Button.tsx
+++ packages/ui/react/src/components/Buttons/Button/Button.tsx
@@ -1,41 +1,175 @@
-import './button.css'
+import type { ButtonHTMLAttributes } from 'react'
 
-export type ButtonProps = {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean
-  /** What background color to use */
+import { BaseButton, ContentLoading, ContentIcon, Label } from './styled'
+import { getColor } from 'src/colors/getColor'
+import type { Icon } from 'src/icons/iconLoader'
+import { iconLoader } from 'src/icons/iconLoader'
+
+const DEFAULT_ICON_SIZE = 16
+const MIN_LOADING_ICON_SIZE = 2
+const LOADING_ICON_SIZE_RATIO = 8
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  /**
+   * By default the button component is 34px high, the "typeSize" property contains
+   * the "medium" variation that changes to 27px
+   */
+  typeSize?: 'default' | 'medium' | 'auto'
+  /** Icon name */
+  icon?: Icon
+  /** Enable button loading animation */
+  loading?: boolean
+  /**
+   * With the property "revertOrientation" it is possible to change the positioning of
+   * the elements inside the button as "icon" or "loading"
+   */
+  revertOrientation?: boolean
+  /** Predefined style variations for the button */
+  buttonType?:
+    | 'borderless'
+    | 'outlinedSecondary'
+    | 'outlinedAuxiliary'
+    | 'Default'
+  /** `Warning`: This property overrides the style defined by the `buttonType` property!
+   *
+   * Customize elements color
+   */
+  color?: string
+  /** `Warning`: This property overrides the style defined by the `buttonType` property!
+   *
+   * Customize background color
+   */
   backgroundColor?: string
-  /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large'
-  /** Button contents */
-  label: string
-  /** Optional click handler */
-  onClick?: () => void
+  /** `Warning`: This property overrides the style defined by the `buttonType` property!
+   *
+   * Customize border color
+   */
+  borderColor?: string
+  /**
+   * The button component has a 15px margin on its sides, to remove activate the "noMargin" property
+   */
+  noMargin?: boolean
+  /**
+   * The button component has a 10px margin on its sides, to remove activate the "noIconMargin" property
+   */
+  noIconMargin?: boolean
+  /**
+   * The button component has a 15px padding on its sides, to remove activate the "noPadding" property
+   */
+  noPadding?: boolean
+  /**
+   * It should be true if the button is wrapped in a tooltip
+   */
+  noPointerEvents?: boolean
+  /**
+   * By default the icon component is 16px high, use this prop to customize "iconSize"
+   */
+  iconSize?: number
+  /**
+   * By default the icon color is primary, use this prop to customize "iconColor"
+   */
+  iconColor?: ReturnType<typeof getColor>
+}
+
+const getTextColor = (buttonType?: string) => {
+  switch (buttonType) {
+    case 'borderless':
+    case 'outlinedSecondary':
+    case 'outlinedAuxiliary':
+      return getColor('purple.800')
+    default:
+      return getColor('white')
+  }
 }
 
 /** Primary UI component for user interaction */
-export const Button = ({
-  primary = false,
-  size = 'medium',
+const Button = ({
+  type,
+  typeSize = 'default',
+  buttonType,
+  color,
   backgroundColor,
-  label,
+  borderColor,
+  disabled,
+  onClick,
+  tabIndex,
+  loading,
+  icon,
+  children,
+  revertOrientation = false,
+  noMargin,
+  noIconMargin,
+  noPointerEvents,
+  iconSize,
+  iconColor,
   ...props
 }: ButtonProps) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary'
+  const handleLoadingAndIcons = ({
+    noIconMargin = false,
+    revertOrientation = false,
+  }) => {
+    if (loading) {
+      return (
+        <ContentLoading
+          data-testid="loading"
+          revertOrientation={revertOrientation}
+          iconSize={iconSize ?? DEFAULT_ICON_SIZE}
+        >
+          {iconLoader(
+            'loading',
+            Math.max(
+              (iconSize ?? DEFAULT_ICON_SIZE) / LOADING_ICON_SIZE_RATIO,
+              MIN_LOADING_ICON_SIZE
+            ),
+            getTextColor(buttonType)
+          )}
+        </ContentLoading>
+      )
+    }
+
+    if (icon) {
+      return (
+        <ContentIcon
+          data-testid="icon"
+          revertOrientation={revertOrientation}
+          noIconMargin={noIconMargin}
+          iconSize={iconSize ?? DEFAULT_ICON_SIZE}
+        >
+          {iconLoader(icon, iconSize ?? DEFAULT_ICON_SIZE, iconColor)}
+        </ContentIcon>
+      )
+    }
+  }
+
   return (
-    <button
-      type="button"
-      className={[
-        'storybook-button',
-        `storybook-button--${size}`,
-        mode,
-      ].join(' ')}
-      style={{ backgroundColor }}
+    <BaseButton
+      data-testid="button"
+      size={typeSize}
+      buttonType={buttonType}
+      reversed={revertOrientation}
+      noPointerEvents={noPointerEvents}
+      custom={{
+        color: color,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+      }}
+      noMargin={noMargin}
+      disabled={disabled}
+      type={type}
+      onClick={(event) => {
+        if (onClick) {
+          onClick(event)
+        }
+      }}
+      tabIndex={tabIndex}
       {...props}
     >
-      {label}
-    </button>
+      {(!!loading || !!icon) &&
+        handleLoadingAndIcons({ noIconMargin, revertOrientation })}
+      {children && <Label className="translate">{children}</Label>}
+    </BaseButton>
   )
 }
+
+export type { ButtonProps }
+export { Button }
