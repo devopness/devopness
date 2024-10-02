6999bf971bf3d17cb7f573cf560669d46a5af469
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-02 11:17:26 -0300

    feat: update js component to type safe version

--- packages/ui/react/src/icons/CircleBubble.tsx
+++ packages/ui/react/src/icons/CircleBubble.tsx
@@ -3,14 +3,20 @@ import { BubbleSpinLoader } from 'react-css-loaders'
 import styled from 'styled-components'
 
 const BubbleSpinLoaderCustom = styled(BubbleSpinLoader)`
-  color: ${(props) => props.color ?? '#000'};
-  fill: ${(props) => props.color ?? '#000'};
+  color: ${(props) => props.color as string | undefined ?? '#000'};
+  fill: ${(props) => props.color as string | undefined ?? '#000'};
   &&& {
     margin: 5px;
   }
 `
 
-const CircleBubble = ({ color, ...restProps }: { color: string }) => (
+const CircleBubble = ({
+  color,
+  ...restProps
+}: {
+  color: string
+  size: number
+}) => (
   <BubbleSpinLoaderCustom
     color={color}
     {...restProps}
