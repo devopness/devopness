52c9cc367711edec4b96320076d0a1cf71242219
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-03 02:26:52 -0300

    feat: update rollup config

--- packages/ui/react/vite.config.ts
+++ packages/ui/react/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     rollupOptions: {
       external: [
         'react',
-        'react-dom',
+        'react/jsx-runtime'
       ],
       output: {
         globals: {
