7486d9791d3af07fb9c133a235408b03fa148c3a
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-03 12:07:13 -0300

    feat: remove unused declare module

--- packages/ui/react/src/global.d.ts
+++
@@ -1,5 +0,0 @@
-// this file is mainly useful while we're incrementally migrating from JavaScript to TypeScript
-// use this file to declare modules for imported packages that do not provide type declaration files
-// if the libraries mentioned here eventually provide type definition files, the respective
-// module can safely be removed from there
-declare module 'react-css-loaders'
