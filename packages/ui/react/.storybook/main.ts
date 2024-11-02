55685e8bf708fb5754ee956b7e944ccf9315cca1
Thales Menezes <thlmenezes.dev@gmail.com>
2024-11-02 20:25:29 -0300

    feat: update storybook config

--- packages/ui/react/.storybook/main.ts
+++ packages/ui/react/.storybook/main.ts
@@ -1,19 +1,28 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: [
     '../src/components/**/*.stories.@(ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-essentials',
-  ],
+
+  addons: [getAbsolutePath("@storybook/addon-essentials"), "@chromatic-com/storybook"],
+
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+
   typescript: {
     check: false,
+    reactDocgen: "react-docgen-typescript"
   },
+
+  docs: {}
 }
 
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
