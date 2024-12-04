dea86eb1b1a9d2fd9003dee5f65bde38bf95e059
Thales Menezes <thlmenezes.dev@gmail.com>
2024-12-04 14:05:05 -0300

    fix: vitest config

--- packages/ui/react/vitest.config.ts
+++ packages/ui/react/vitest.config.ts
@@ -2,10 +2,10 @@ import { defineConfig, mergeConfig } from 'vitest/config'
 import { resolve } from 'path'
 import viteConfig from './vite.config'
 
-export default mergeConfig(viteConfig, {
+export default mergeConfig(viteConfig, defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: resolve(__dirname, 'src/setupTest.ts'),
   },
-})
+}))
