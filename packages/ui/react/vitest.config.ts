a27fbf5b2407b1076597a9b15690d93973be0931
Thales Menezes <thlmenezes.dev@gmail.com>
2024-12-04 14:06:06 -0300

    fix: lint errors

--- packages/ui/react/vitest.config.ts
+++ packages/ui/react/vitest.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
 import { resolve } from 'path'
+import { defineConfig, mergeConfig } from 'vitest/config'
+
 import viteConfig from './vite.config'
 
-export default mergeConfig(viteConfig, defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: resolve(__dirname, 'src/setupTest.ts'),
-  },
-}))
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: resolve(__dirname, 'src/setupTest.ts'),
+    },
+  })
+)
