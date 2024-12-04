7d33de62bf4c4d49947aea9d31e83d8a174b3df0
Thales Menezes <thlmenezes.dev@gmail.com>
2024-12-04 13:55:25 -0300

    feat: bump vitest version

---
+++ packages/ui/react/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { resolve } from 'path'
+import viteConfig from './vite.config'
+
+export default mergeConfig(viteConfig, {
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: resolve(__dirname, 'src/setupTest.ts'),
+  },
+})
