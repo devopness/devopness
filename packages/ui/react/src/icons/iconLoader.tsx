310935daa70572b9bbdabfa0a7bd79f008c71cf7
Thales Menezes <thlmenezes.dev@gmail.com>
2025-04-02 15:17:30 -0300

    feat(icons): update iconLoader with new icons and reorganize existing ones
    - Added new SVG icons for AWS, Azure, CentOS, DigitalOcean, GCP, Java, and others.
    - Reorganized existing icons for better structure and clarity.
    - Removed duplicate icon entries and ensured all icons are correctly referenced in the icon list.

--- packages/ui/react/src/icons/iconLoader.tsx
+++ packages/ui/react/src/icons/iconLoader.tsx
@@ -74,161 +74,143 @@ import { RxOpenInNewWindow } from 'react-icons/rx'
 import { CircleBubble } from './CircleBubble'
 import { getImageAssetUrl } from './getImageAssetUrl'
 
+const awsSVG = getImageAssetUrl('icons_svgs/aws.svg')
+const azureSVG = getImageAssetUrl('icons_svgs/azure.svg')
 const bitbucketSVG = getImageAssetUrl('icons_svgs/bitbucket.svg')
 const bitbucketWhiteSVG = getImageAssetUrl('icons_svgs/bitbucket-white.svg')
+const centosSVG = getImageAssetUrl('icons_svgs/centos.svg')
 const cSharpSVG = getImageAssetUrl('icons_svgs/c-sharp.svg')
+const devopnessSymbolSVG = getImageAssetUrl('logo-symbol-devopness.svg')
+const digitaloceanSVG = getImageAssetUrl('icons_svgs/digitalocean.svg')
 const dockerSVG = getImageAssetUrl('icons_svgs/docker.svg')
+const dotnetCoreAspnetCoreSVG = getImageAssetUrl('icons_svgs/dotnetcore-aspnetcore.svg')
 const dotnetCoreSVG = getImageAssetUrl('icons_svgs/dotnetcore.svg')
-const dotnetCoreAspnetCoreSVG = getImageAssetUrl(
-  'icons_svgs/dotnetcore-aspnetcore.svg'
-)
+const gcpSVG = getImageAssetUrl('icons_svgs/gcp.svg')
 const githubSVG = getImageAssetUrl('icons_svgs/github.svg')
 const githubWhiteSVG = getImageAssetUrl('icons_svgs/github-white.svg')
 const gitlabSVG = getImageAssetUrl('icons_svgs/gitlab.svg')
 const htmlSVG = getImageAssetUrl('icons_svgs/html.svg')
-const nodejsSVG = getImageAssetUrl('icons_svgs/nodejs.svg')
-const phpSVG = getImageAssetUrl('icons_svgs/php.svg')
-const phpLaravelSVG = getImageAssetUrl('icons_svgs/php-laravel.svg')
-const pythonSVG = getImageAssetUrl('icons_svgs/python.svg')
-const pythonDjangoSVG = getImageAssetUrl('icons_svgs/python-django.svg')
-const pythonFlaskSVG = getImageAssetUrl('icons_svgs/python-flask.svg')
-const pythonFastAPISVG = getImageAssetUrl('icons_svgs/python-fastapi.svg')
-const ubuntuSVG = getImageAssetUrl('icons_svgs/ubuntu.svg')
-const centosSVG = getImageAssetUrl('icons_svgs/centos.svg')
-const awsSVG = getImageAssetUrl('icons_svgs/aws.svg')
-const gcpSVG = getImageAssetUrl('icons_svgs/gcp.svg')
-const azureSVG = getImageAssetUrl('icons_svgs/azure.svg')
-const digitaloceanSVG = getImageAssetUrl('icons_svgs/digitalocean.svg')
-const typeHierarchySubSVG = getImageAssetUrl(
-  'icons_svgs/type-hierarchy-sub.svg'
-)
-const typeHierarchySuperSVG = getImageAssetUrl(
-  'icons_svgs/type-hierarchy-super.svg'
-)
 const javaSVG = getImageAssetUrl('icons_svgs/java.svg')
+const mdOutlineShieldLockSVG = getImageAssetUrl('icons_svgs/outline-shield-lock.svg')
+const nodejsSVG = getImageAssetUrl('icons_svgs/nodejs.svg')
+const phpLaravelSVG = getImageAssetUrl('icons_svgs/php-laravel.svg')
+const phpSVG = getImageAssetUrl('icons_svgs/php.svg')
+const pythonDjangoSVG = getImageAssetUrl('icons_svgs/python-django.svg')
+const pythonFastAPISVG = getImageAssetUrl('icons_svgs/python-fastapi.svg')
+const pythonFlaskSVG = getImageAssetUrl('icons_svgs/python-flask.svg')
+const pythonSVG = getImageAssetUrl('icons_svgs/python.svg')
 const rubySVG = getImageAssetUrl('icons_svgs/ruby.svg')
-const mdOutlineShieldLockSVG = getImageAssetUrl(
-  'icons_svgs/outline-shield-lock.svg'
-)
-
-const devopnessSymbolSVG = getImageAssetUrl('logo-symbol-devopness.svg')
+const typeHierarchySubSVG = getImageAssetUrl('icons_svgs/type-hierarchy-sub.svg')
+const typeHierarchySuperSVG = getImageAssetUrl('icons_svgs/type-hierarchy-super.svg')
+const ubuntuSVG = getImageAssetUrl('icons_svgs/ubuntu.svg')
 
 const iconList = [
-  { type: 'icon', accessor: 'organization', component: GoOrganization },
-  { type: 'icon', accessor: 'link', component: AiOutlineLink },
-  { type: 'icon', accessor: 'unlink', component: BiUnlink },
-  { type: 'icon', accessor: 'tag', component: PiTagDuotone },
-  { type: 'icon', accessor: 'help', component: BsQuestionCircleFill },
-  { type: 'icon', accessor: 'key', component: GoKey },
-  { type: 'icon', accessor: 'skip', component: GoSkip },
-  { type: 'icon', accessor: 'pending', component: MdPending },
-  { type: 'icon', accessor: 'loading', component: CircleBubble },
-  { type: 'icon', accessor: 'server', component: FaServer },
-  { type: 'icon', accessor: 'self-hosted', component: FaServer },
-  { type: 'icon', accessor: 'sortIcon', component: FaSort },
   { type: 'icon', accessor: 'add', component: MdAdd },
-  { type: 'icon', accessor: 'plus', component: FaPlus },
-  { type: 'icon', accessor: 'downArrow', component: MdKeyboardArrowDown },
-  { type: 'icon', accessor: 'upArrow', component: MdKeyboardArrowUp },
-  { type: 'icon', accessor: 'leftArrow', component: MdKeyboardArrowLeft },
-  { type: 'icon', accessor: 'rightArrow', component: MdKeyboardArrowRight },
-  { type: 'icon', accessor: 'config', component: FaCog },
-  { type: 'icon', accessor: 'userCheck', component: FaUserCheck },
-  { type: 'icon', accessor: 'userClock', component: FaUserClock },
-  { type: 'icon', accessor: 'userTimes', component: FaUserTimes },
-  { type: 'icon', accessor: 'warningCloud', component: PiCloudWarningBold },
-  { type: 'icon', accessor: 'error', component: MdError },
-  { type: 'icon', accessor: 'warning', component: MdWarning },
-  { type: 'icon', accessor: 'checkCircle', component: MdCheckCircle },
+  { type: 'icon', accessor: 'aiOutlineDollarCircle', component: AiOutlineDollarCircle },
   { type: 'icon', accessor: 'alarmOn', component: MdAlarmOn },
-  { type: 'icon', accessor: 'dragHandle', component: MdDragHandle },
-  { type: 'icon', accessor: 'delete', component: MdDelete },
-  { type: 'icon', accessor: 'edit', component: MdEdit },
-  { type: 'icon', accessor: 'lockOpen', component: FaLockOpen },
-  { type: 'icon', accessor: 'lock', component: FaLock },
-  { type: 'icon', accessor: 'eye2Line', component: RiEye2Line },
-  { type: 'icon', accessor: 'cubes', component: FaCubes },
-  { type: 'icon', accessor: 'security', component: MdSecurity },
-  { type: 'icon', accessor: 'fillGearFill', component: BsFillGearFill },
-  { type: 'icon', accessor: 'devices', component: MdDevices },
-  { type: 'icon', accessor: 'logout', component: FiLogOut },
-  { type: 'icon', accessor: 'codeBadge', component: DiCodeBadge },
-  { type: 'icon', accessor: 'logout', component: FiLogOut },
-  { type: 'icon', accessor: 'snooze', component: MdSnooze },
-  { type: 'icon', accessor: 'timeroff', component: MdTimerOff },
-  { type: 'icon', accessor: 'deploy', component: AiOutlineDeploymentUnit },
-  { type: 'icon', accessor: 'outlineCheck', component: AiOutlineCheck },
-  { type: 'icon', accessor: 'filledCheck', component: RiShieldCheckFill },
-  { type: 'icon', accessor: 'settings', component: MdSettings },
+  { type: 'image', accessor: 'aws', component: awsSVG },
+  { type: 'image', accessor: 'azure', component: azureSVG },
+  { type: 'image', accessor: 'bitbucket', component: bitbucketSVG },
+  { type: 'image', accessor: 'bitbucket-white', component: bitbucketWhiteSVG },
   { type: 'icon', accessor: 'branch', component: FiGitBranch },
-  { type: 'icon', accessor: 'commit', component: GoGitCommit },
-  { type: 'icon', accessor: 'user', component: MdPersonOutline },
-  { type: 'icon', accessor: 'copy', component: FaCopy },
-  { type: 'icon', accessor: 'outlineCopy', component: FaRegCopy },
-  { type: 'icon', accessor: 'lens', component: MdLens },
-  { type: 'icon', accessor: 'more', component: MdMoreHoriz },
-  { type: 'icon', accessor: 'launch', component: GrLaunch },
-  { type: 'icon', accessor: 'connect', component: GrConnect },
+  { type: 'image', accessor: 'c-sharp', component: cSharpSVG },
+  { type: 'image', accessor: 'centos', component: centosSVG },
+  { type: 'icon', accessor: 'checkCircle', component: MdCheckCircle },
+  { type: 'icon', accessor: 'children', component: typeHierarchySuperSVG },
+  { type: 'icon', accessor: 'close', component: GrClose },
   { type: 'icon', accessor: 'cloudServer', component: AiOutlineCloudServer },
-  { type: 'icon', accessor: 'gitBranch', component: FiGitBranch },
-  { type: 'icon', accessor: 'outlineServer', component: FiServer },
-  { type: 'icon', accessor: 'storage', component: FaDatabase },
-  { type: 'icon', accessor: 'mapPinLine', component: RiMapPinLine },
-  { type: 'icon', accessor: 'outlineSafety', component: AiOutlineSafety },
-  { type: 'icon', accessor: 'plusCircle', component: FiPlusCircle },
-  { type: 'icon', accessor: 'riTeamLine', component: RiTeamLine },
-  {
-    type: 'icon',
-    accessor: 'aiOutlineDollarCircle',
-    component: AiOutlineDollarCircle,
-  },
-  { type: 'icon', accessor: 'network', component: LiaNetworkWiredSolid },
-  { type: 'icon', accessor: 'subnet', component: ImTree },
-  { type: 'icon', accessor: 'mdGroup', component: MdGroup },
+  { type: 'icon', accessor: 'codeBadge', component: DiCodeBadge },
+  { type: 'icon', accessor: 'commit', component: GoGitCommit },
+  { type: 'icon', accessor: 'config', component: FaCog },
+  { type: 'icon', accessor: 'connect', component: GrConnect },
+  { type: 'icon', accessor: 'copy', component: FaCopy },
+  { type: 'icon', accessor: 'cubes', component: FaCubes },
+  { type: 'icon', accessor: 'delete', component: MdDelete },
+  { type: 'icon', accessor: 'deploy', component: AiOutlineDeploymentUnit },
+  { type: 'icon', accessor: 'description', component: MdOutlineDescription },
+  { type: 'image', accessor: 'devopness', component: devopnessSymbolSVG },
+  { type: 'icon', accessor: 'devices', component: MdDevices },
+  { type: 'image', accessor: 'digitalocean', component: digitaloceanSVG },
+  { type: 'image', accessor: 'docker', component: dockerSVG },
+  { type: 'image', accessor: 'dotnetcore', component: dotnetCoreSVG },
+  { type: 'image', accessor: 'dotnetcore-aspnetcore', component: dotnetCoreAspnetCoreSVG },
+  { type: 'icon', accessor: 'downArrow', component: MdKeyboardArrowDown },
+  { type: 'icon', accessor: 'dragHandle', component: MdDragHandle },
+  { type: 'icon', accessor: 'edit', component: MdEdit },
+  { type: 'icon', accessor: 'error', component: MdError },
+  { type: 'icon', accessor: 'eye', component: IoMdEye },
+  { type: 'icon', accessor: 'eye2Line', component: RiEye2Line },
+  { type: 'icon', accessor: 'eyeOff', component: IoMdEyeOff },
   { type: 'icon', accessor: 'faGlobe', component: FaGlobe },
-  { type: 'icon', accessor: 'openInNewWindow', component: RxOpenInNewWindow },
+  { type: 'icon', accessor: 'fillGearFill', component: BsFillGearFill },
+  { type: 'icon', accessor: 'filledCheck', component: RiShieldCheckFill },
+  { type: 'icon', accessor: 'folder', component: MdOutlineFolder },
+  { type: 'image', accessor: 'gcp', component: gcpSVG },
+  { type: 'icon', accessor: 'gitBranch', component: FiGitBranch },
   { type: 'image', accessor: 'github', component: githubSVG },
   { type: 'image', accessor: 'github-white', component: githubWhiteSVG },
   { type: 'image', accessor: 'gitlab', component: gitlabSVG },
-  { type: 'image', accessor: 'bitbucket', component: bitbucketSVG },
-  { type: 'image', accessor: 'bitbucket-white', component: bitbucketWhiteSVG },
-  { type: 'image', accessor: 'docker', component: dockerSVG },
-  { type: 'image', accessor: 'c-sharp', component: cSharpSVG },
-  { type: 'image', accessor: 'dotnetcore', component: dotnetCoreSVG },
-  {
-    type: 'image',
-    accessor: 'dotnetcore-aspnetcore',
-    component: dotnetCoreAspnetCoreSVG,
-  },
+  { type: 'icon', accessor: 'help', component: BsQuestionCircleFill },
   { type: 'image', accessor: 'html', component: htmlSVG },
+  { type: 'icon', accessor: 'info', component: MdInfo },
+  { type: 'image', accessor: 'java', component: javaSVG },
+  { type: 'icon', accessor: 'key', component: GoKey },
+  { type: 'icon', accessor: 'launch', component: GrLaunch },
+  { type: 'icon', accessor: 'leftArrow', component: MdKeyboardArrowLeft },
+  { type: 'icon', accessor: 'lens', component: MdLens },
+  { type: 'icon', accessor: 'link', component: AiOutlineLink },
+  { type: 'icon', accessor: 'loading', component: CircleBubble },
+  { type: 'icon', accessor: 'lock', component: FaLock },
+  { type: 'icon', accessor: 'lockOpen', component: FaLockOpen },
+  { type: 'icon', accessor: 'logout', component: FiLogOut },
+  { type: 'icon', accessor: 'mapPinLine', component: RiMapPinLine },
+  { type: 'icon', accessor: 'mdGroup', component: MdGroup },
+  { type: 'icon', accessor: 'more', component: MdMoreHoriz },
+  { type: 'icon', accessor: 'network', component: LiaNetworkWiredSolid },
   { type: 'image', accessor: 'nodejs', component: nodejsSVG },
+  { type: 'icon', accessor: 'openInNewWindow', component: RxOpenInNewWindow },
+  { type: 'icon', accessor: 'organization', component: GoOrganization },
+  { type: 'icon', accessor: 'outlineCheck', component: AiOutlineCheck },
+  { type: 'icon', accessor: 'outlineCopy', component: FaRegCopy },
+  { type: 'icon', accessor: 'outlineSafety', component: AiOutlineSafety },
+  { type: 'icon', accessor: 'outlineServer', component: FiServer },
+  { type: 'icon', accessor: 'parent', component: typeHierarchySubSVG },
+  { type: 'icon', accessor: 'passkey', component: GoPasskeyFill },
+  { type: 'icon', accessor: 'pending', component: MdPending },
   { type: 'image', accessor: 'php', component: phpSVG },
   { type: 'image', accessor: 'php-laravel', component: phpLaravelSVG },
+  { type: 'icon', accessor: 'plus', component: FaPlus },
+  { type: 'icon', accessor: 'plusCircle', component: FiPlusCircle },
   { type: 'image', accessor: 'python', component: pythonSVG },
   { type: 'image', accessor: 'python-django', component: pythonDjangoSVG },
-  { type: 'image', accessor: 'python-flask', component: pythonFlaskSVG },
   { type: 'image', accessor: 'python-fastapi', component: pythonFastAPISVG },
-  { type: 'image', accessor: 'ubuntu', component: ubuntuSVG },
-  { type: 'image', accessor: 'centos', component: centosSVG },
-  { type: 'image', accessor: 'aws', component: awsSVG },
-  { type: 'image', accessor: 'azure', component: azureSVG },
-  { type: 'image', accessor: 'digitalocean', component: digitaloceanSVG },
-  { type: 'image', accessor: 'gcp', component: gcpSVG },
-  { type: 'image', accessor: 'parent', component: typeHierarchySubSVG },
-  { type: 'image', accessor: 'children', component: typeHierarchySuperSVG },
-  { type: 'image', accessor: 'java', component: javaSVG },
+  { type: 'image', accessor: 'python-flask', component: pythonFlaskSVG },
+  { type: 'icon', accessor: 'rightArrow', component: MdKeyboardArrowRight },
+  { type: 'icon', accessor: 'riTeamLine', component: RiTeamLine },
   { type: 'image', accessor: 'ruby', component: rubySVG },
+  { type: 'icon', accessor: 'security', component: MdSecurity },
+  { type: 'icon', accessor: 'self-hosted', component: FaServer },
+  { type: 'icon', accessor: 'server', component: FaServer },
+  { type: 'icon', accessor: 'settings', component: MdSettings },
   { type: 'image', accessor: 'shieldLock', component: mdOutlineShieldLockSVG },
-  { type: 'image', accessor: 'devopness', component: devopnessSymbolSVG },
-  { type: 'icon', accessor: 'close', component: GrClose },
-  { type: 'icon', accessor: 'eye', component: IoMdEye },
-  { type: 'icon', accessor: 'eyeOff', component: IoMdEyeOff },
-  { type: 'icon', accessor: 'passkey', component: GoPasskeyFill },
-  { type: 'icon', accessor: 'info', component: MdInfo },
-  { type: 'icon', accessor: 'description', component: MdOutlineDescription },
+  { type: 'icon', accessor: 'skip', component: GoSkip },
+  { type: 'icon', accessor: 'snooze', component: MdSnooze },
+  { type: 'icon', accessor: 'sortIcon', component: FaSort },
+  { type: 'icon', accessor: 'storage', component: FaDatabase },
+  { type: 'icon', accessor: 'subnet', component: ImTree },
+  { type: 'icon', accessor: 'tag', component: PiTagDuotone },
   { type: 'icon', accessor: 'terminal', component: BsTerminal },
+  { type: 'icon', accessor: 'timeroff', component: MdTimerOff },
   { type: 'icon', accessor: 'treeView', component: PiTreeView },
-  { type: 'icon', accessor: 'folder', component: MdOutlineFolder },
+  { type: 'image', accessor: 'ubuntu', component: ubuntuSVG },
+  { type: 'icon', accessor: 'unlink', component: BiUnlink },
+  { type: 'icon', accessor: 'upArrow', component: MdKeyboardArrowUp },
+  { type: 'icon', accessor: 'user', component: MdPersonOutline },
+  { type: 'icon', accessor: 'userCheck', component: FaUserCheck },
+  { type: 'icon', accessor: 'userClock', component: FaUserClock },
+  { type: 'icon', accessor: 'userTimes', component: FaUserTimes },
+  { type: 'icon', accessor: 'warning', component: MdWarning },
+  { type: 'icon', accessor: 'warningCloud', component: PiCloudWarningBold },
 ] as const
 
 /**
