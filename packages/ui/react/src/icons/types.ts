c8a1811f10c675a5412999170ca3b82ebaaa8197
Thales Menezes <thlmenezes.dev@gmail.com>
2025-04-02 22:22:07 -0300

    refactor(icons): update type definitions for icon names
    - Changed documentation comments to clarify that the type definitions now refer to valid icon names instead of accessors.
    - Updated references to deprecated icons to align with the new naming convention, enhancing clarity and consistency in the codebase.

--- packages/ui/react/src/icons/types.ts
+++ packages/ui/react/src/icons/types.ts
@@ -1,15 +1,15 @@
 import type { deprecatedToNewIconMap, iconList } from './iconLoader'
 
 /**
- * Lists all valid icon accessors available in the iconList
+ * Lists all valid icon names available in the iconList
  */
 type Icon = (typeof iconList)[number]['name']
 
 /**
- * Lists all valid icon accessors, filtered out the deprecated ones
+ * Lists all valid icon names, filtered out the deprecated ones
  *
  * If you need to migrate from a deprecated icon, you can use the
- * DeprecatedIcons object to find the new icon accessor.
+ * deprecatedToNewIconMap object to find the new icon name.
  *
  * @see {deprecatedToNewIconMap}
  */
