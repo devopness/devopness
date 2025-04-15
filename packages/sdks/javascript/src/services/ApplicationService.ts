00cefb62fd0f3c5cbc4abb398683a8d7330a94ae
Diegiwg <diegiwg@gmail.com>
2025-04-15 10:40:57 -0300

    refactor: remove deprecated services from Application and Environment

--- packages/sdks/javascript/src/services/ApplicationService.ts
+++ packages/sdks/javascript/src/services/ApplicationService.ts
@@ -1,7 +1,6 @@
 import { ApiBaseService } from './ApiBaseService';
 import { mergeSiblingClasses } from '../common/MergeSiblingClasses';
 import { ApplicationsApiService } from '../api/generated/apis/applications-api';
-import { ApplicationsDeploymentsApiService } from '../api/generated/apis/applications-deployments-api';
 import { ApplicationsVariablesApiService } from '../api/generated/apis/applications-variables-api';
 import { ApplicationsHooksApiService } from '../api/generated/apis/applications-hooks-api';
 
@@ -10,15 +9,12 @@ export class ApplicationService extends ApiBaseService { }
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface ApplicationService extends
     ApplicationsApiService,
-    ApplicationsDeploymentsApiService,
-    ApplicationsApiService,
     ApplicationsVariablesApiService,
     ApplicationsHooksApiService {
 }
 
 mergeSiblingClasses(ApplicationService, [
     ApplicationsApiService,
-    ApplicationsDeploymentsApiService,
     ApplicationsVariablesApiService,
     ApplicationsHooksApiService,
 ]);
--- packages/sdks/javascript/src/services/EnvironmentService.ts
+++ packages/sdks/javascript/src/services/EnvironmentService.ts
@@ -1,32 +1,10 @@
 import { EnvironmentsActionsApiService } from '../api/generated/apis/environments-actions-api';
 import { EnvironmentsApiService } from '../api/generated/apis/environments-api';
-import { EnvironmentsApplicationsApiService } from '../api/generated/apis/environments-applications-api';
-import { EnvironmentsCredentialsApiService } from '../api/generated/apis/environments-credentials-api';
-import { EnvironmentsCronJobsApiService } from '../api/generated/apis/environments-cron-jobs-api';
-import { EnvironmentsDaemonsApiService } from '../api/generated/apis/environments-daemons-api';
-import { EnvironmentsNetworkRulesApiService } from '../api/generated/apis/environments-network-rules-api';
-import { EnvironmentsNetworksApiService } from '../api/generated/apis/environments-networks-api';
-import { EnvironmentsServersApiService } from '../api/generated/apis/environments-servers-api';
-import { EnvironmentsServicesApiService } from '../api/generated/apis/environments-services-api';
-import { EnvironmentsSSHKeysApiService } from '../api/generated/apis/environments-sshkeys-api';
-import { EnvironmentsSSLCertificatesApiService } from '../api/generated/apis/environments-sslcertificates-api';
 import { EnvironmentsTeamMembershipsApiService } from '../api/generated/apis/environments-team-memberships-api';
 import { EnvironmentsTeamsApiService } from '../api/generated/apis/environments-teams-api';
-import { EnvironmentsVirtualHostsApiService } from '../api/generated/apis/environments-virtual-hosts-api';
 
 export class EnvironmentService extends EnvironmentsApiService {
     public actions = new EnvironmentsActionsApiService();
-    public applications = new EnvironmentsApplicationsApiService();
-    public credentials = new EnvironmentsCredentialsApiService();
-    public cronjobs = new EnvironmentsCronJobsApiService();
-    public daemons = new EnvironmentsDaemonsApiService();
-    public networks = new EnvironmentsNetworksApiService();
-    public networkRules = new EnvironmentsNetworkRulesApiService();
-    public servers = new EnvironmentsServersApiService();
-    public services = new EnvironmentsServicesApiService();
-    public sshKeys = new EnvironmentsSSHKeysApiService();
-    public sslCertificates = new EnvironmentsSSLCertificatesApiService();
     public teams = new EnvironmentsTeamsApiService();
     public teamMemberships = new EnvironmentsTeamMembershipsApiService();
-    public virtualHosts = new EnvironmentsVirtualHostsApiService();
 }
