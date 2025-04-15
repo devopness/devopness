f30e7be9d66de180a4e2caef2acb5cc190104729
Devopness Automations <114699329+devopness-automations@users.noreply.github.com>
2025-04-15 22:12:13 +0100

    feat: 2025-04-15 13:28 - Update API Specification (#1587)
    * feat: update 'spec.json' file
    
    * refactor: remove deprecated services from Application and Environment
    
    * docs: Update auto generated models
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Diegiwg <diegiwg@gmail.com>
    Co-authored-by: Diegiwg <Diegiwg@users.noreply.github.com>

--- packages/sdks/javascript/src/api/generated/apis/services-api.ts
+++ packages/sdks/javascript/src/api/generated/apis/services-api.ts
@@ -16,7 +16,9 @@ import { ApiResponse } from "../../../common/ApiResponse";
 import { ArgumentNullException } from "../../../common/Exceptions";
 import { ApiError } from '../../generated/models';
 import { Service } from '../../generated/models';
+import { ServiceEnvironmentCreate } from '../../generated/models';
 import { ServiceGetStatus } from '../../generated/models';
+import { ServiceRelation } from '../../generated/models';
 import { ServiceReload } from '../../generated/models';
 import { ServiceRestart } from '../../generated/models';
 import { ServiceStart } from '../../generated/models';
@@ -27,6 +29,28 @@ import { ServiceUpdate } from '../../generated/models';
  * ServicesApiService - Auto-generated
  */
 export class ServicesApiService extends ApiBaseService {
+    /**
+     * 
+     * @summary Add a Service to the given environment
+     * @param {number} environmentId The ID of the environment.
+     * @param {ServiceEnvironmentCreate} serviceEnvironmentCreate A JSON object containing the resource data
+     */
+    public async addEnvironmentService(environmentId: number, serviceEnvironmentCreate: ServiceEnvironmentCreate): Promise<ApiResponse<Service>> {
+        if (environmentId === null || environmentId === undefined) {
+            throw new ArgumentNullException('environmentId', 'addEnvironmentService');
+        }
+        if (serviceEnvironmentCreate === null || serviceEnvironmentCreate === undefined) {
+            throw new ArgumentNullException('serviceEnvironmentCreate', 'addEnvironmentService');
+        }
+
+        let queryString = '';
+
+        const requestUrl = '/environments/{environment_id}/services' + (queryString? `?${queryString}` : '');
+
+        const response = await this.post <Service, ServiceEnvironmentCreate>(requestUrl.replace(`{${"environment_id"}}`, encodeURIComponent(String(environmentId))), serviceEnvironmentCreate);
+        return new ApiResponse(response);
+    }
+
     /**
      * 
      * @summary Delete a given service
@@ -82,6 +106,34 @@ export class ServicesApiService extends ApiBaseService {
         const requestUrl = '/services/{service_id}/get-status' + (queryString? `?${queryString}` : '');
 
         const response = await this.post <void, ServiceGetStatus>(requestUrl.replace(`{${"service_id"}}`, encodeURIComponent(String(serviceId))), serviceGetStatus);
+        return new ApiResponse(response);
+    }
+
+    /**
+     * 
+     * @summary Return a list of all services belonging to a environment
+     * @param {number} environmentId The ID of the environment.
+     * @param {number} [page] Number of the page to be retrieved
+     * @param {number} [perPage] Number of items returned per page
+     */
+    public async listEnvironmentServices(environmentId: number, page?: number, perPage?: number): Promise<ApiResponse<Array<ServiceRelation>>> {
+        if (environmentId === null || environmentId === undefined) {
+            throw new ArgumentNullException('environmentId', 'listEnvironmentServices');
+        }
+
+        let queryString = '';
+        const queryParams = { page: page, per_page: perPage, } as { [key: string]: any };
+        for (const key in queryParams) {
+            if (queryParams[key] === undefined || queryParams[key] === null) {
+                continue;
+            }
+
+            queryString += (queryString? '&' : '') + `${key}=${encodeURI(queryParams[key])}`;
+        }
+
+        const requestUrl = '/environments/{environment_id}/services' + (queryString? `?${queryString}` : '');
+
+        const response = await this.get <Array<ServiceRelation>>(requestUrl.replace(`{${"environment_id"}}`, encodeURIComponent(String(environmentId))));
         return new ApiResponse(response);
     }
 
