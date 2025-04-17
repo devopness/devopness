974d4438d28714aac3738ab856969b420efb6e41
Diegiwg <diegiwg@gmail.com>
2025-04-17 17:41:33 -0300

    feat: remove publish-dev target from Makefile and enhance publish-dev script with token validation and configuration for Test PyPI

--- packages/sdks/python/scripts/publish-dev.sh
+++ packages/sdks/python/scripts/publish-dev.sh
@@ -4,6 +4,14 @@
 
 set -e
 
+if [[ -z "$POETRY_TEST_PYPI_TOKEN" ]]; then
+  echo "🚨  POETRY_TEST_PYPI_TOKEN is not set. Please set it before publishing to Test PyPI."
+  exit 1
+fi
+
+echo "📦  Configuring Test PyPI..."
+poetry config pypi-token.TestPiPy "$POETRY_TEST_PYPI_TOKEN"
+
 echo "📦  Building Devopness SDK - Python..."
 poetry build
 
