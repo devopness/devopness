b377d049855f0e46e272050b097796cd903151b7
Diegiwg <diegiwg@gmail.com>
2025-04-23 16:10:38 -0300

    refactor: use unittest instead of pytest

--- packages/sdks/python/scripts/test.sh
+++ packages/sdks/python/scripts/test.sh
@@ -16,5 +16,5 @@ if [[ $INSTALL_EXIT_CODE -ne 0 ]]; then
   exit $INSTALL_EXIT_CODE
 fi
 
-echo "🧪  Running Tests..."
-poetry run pytest "$PYTEST_ARGS"
+echo "🧪  Running Unit Tests..."
+python -m unittest discover -vv -b -s tests/unit
