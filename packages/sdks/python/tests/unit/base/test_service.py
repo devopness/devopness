5e57cd7e5de326a28d4f06355ce0bb4f64e3a743
Diegiwg <diegiwg@gmail.com>
2025-04-24 16:38:05 -0300

    feat(tests): add tests for removing null keys in POST and PUT requests

--- packages/sdks/python/tests/unit/base/test_service.py
+++ packages/sdks/python/tests/unit/base/test_service.py
@@ -5,6 +5,7 @@ import httpx
 
 from devopness import DevopnessClientConfig
 from devopness._base import DevopnessBaseService
+from devopness.models import ProjectCreate, ProjectUpdate
 
 
 class TestDevopnessBaseService(unittest.TestCase):
@@ -51,6 +52,82 @@ class TestDevopnessBaseService(unittest.TestCase):
         self.assertIn("Authorization", request.headers)
         self.assertEqual(request.headers["Authorization"], "Bearer dp-token123")
 
+    @patch("httpx.Client.send")
+    def test_perform_post_request_with_dict_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = {"name": "John Doe", "age": None}
+        self.service._post_sync("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "POST")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"name":"John Doe"}')
+
+    @patch("httpx.Client.send")
+    def test_perform_post_request_with_sdk_model_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = ProjectCreate(name="Cool Project", organization_id=None)
+        self.service._post_sync("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "POST")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"name":"Cool Project"}')
+
+    @patch("httpx.Client.send")
+    def test_perform_put_request_with_dict_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = {"name": "John Doe", "age": None}
+        self.service._put_sync("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "PUT")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"name":"John Doe"}')
+
+    @patch("httpx.Client.send")
+    def test_perform_put_request_with_sdk_model_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = ProjectUpdate(id=123, name="Cool Project", logo_image=None)
+        self.service._put_sync("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "PUT")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"id":123,"name":"Cool Project"}')
+
 
 class TestDevopnessBaseServiceAsync(unittest.IsolatedAsyncioTestCase):
     config = DevopnessClientConfig(base_url="https://test.local")
@@ -95,3 +172,79 @@ class TestDevopnessBaseServiceAsync(unittest.IsolatedAsyncioTestCase):
 
         self.assertIn("Authorization", request.headers)
         self.assertEqual(request.headers["Authorization"], "Bearer dp-token123")
+
+    @patch("httpx.AsyncClient.send")
+    async def test_perform_post_request_with_dict_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = {"name": "John Doe", "age": None}
+        await self.service._post("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "POST")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"name":"John Doe"}')
+
+    @patch("httpx.AsyncClient.send")
+    async def test_perform_post_request_with_sdk_model_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = ProjectCreate(name="Cool Project", organization_id=None)
+        await self.service._post("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "POST")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"name":"Cool Project"}')
+
+    @patch("httpx.AsyncClient.send")
+    async def test_perform_put_request_with_dict_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = {"name": "John Doe", "age": None}
+        await self.service._put("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "PUT")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"name":"John Doe"}')
+
+    @patch("httpx.AsyncClient.send")
+    async def test_perform_put_request_with_sdk_model_with_keys_unset_expects_this_keys_is_removed_from_payload(
+        self,
+        mock: Mock,
+    ) -> None:
+        payload = ProjectUpdate(id=123, name="Cool Project", logo_image=None)
+        await self.service._put("/resource", payload)
+
+        mock.assert_called_once()
+
+        request: httpx.Request = mock.call_args[0][0]
+        self.assertIsInstance(request, httpx.Request)
+
+        self.assertEqual(request.method, "PUT")
+        self.assertEqual(request.url, "https://test.local/resource")
+
+        self.assertEqual(request.headers["Content-Type"], "application/json")
+        self.assertEqual(request.content, b'{"id":123,"name":"Cool Project"}')
