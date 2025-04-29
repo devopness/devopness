d89bddeabd4a51fc5a0588ca8debd1ff1360df08
Diegiwg <diegiwg@gmail.com>
2025-04-29 16:59:47 -0300

    feat: add network and subnet services to client

--- packages/sdks/python/devopness/services.py
+++ packages/sdks/python/devopness/services.py
@@ -4,14 +4,18 @@ Devopness API Python SDK - Painless essential DevOps to everyone
 
 from ._services.credential_service import CredentialService
 from ._services.environment_service import EnvironmentService
+from ._services.network_service import NetworkService
 from ._services.project_service import ProjectService
 from ._services.server_service import ServerService
+from ._services.subnet_service import SubnetService
 from ._services.user_service import UserService
 
 __all__ = [
     "CredentialService",
     "EnvironmentService",
+    "NetworkService",
     "ProjectService",
     "ServerService",
+    "SubnetService",
     "UserService",
 ]
