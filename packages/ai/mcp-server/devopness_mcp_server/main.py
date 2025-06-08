e748625cf9ad9d77cbbaf6e63eaaeeca90cb59d7
Diegiwg <diegiwg@gmail.com>
2025-06-08 12:46:09 -0300

    refactor: rename 'src' to' 'devopness_mcp_server' package

---
+++ packages/ai/mcp-server/devopness_mcp_server/main.py
@@ -0,0 +1,16 @@
+from .server import server
+
+
+def run() -> None:
+    """
+    Start the Devopness MCP Server.
+
+    This is the main entry-point for the MCP server.
+
+    It sets up the server, registers the tools and then starts the server.
+    """
+    server.run()
+
+
+if __name__ == "__main__":
+    run()
