2abd759454ca4596854c594335d29b0da05f845f
Willian Santos <williansantosd2@gmail.com>
2025-09-12 17:55:22 -0300

    feat: add AccordionExpand component

---
+++ packages/ui/react/src/components/Primitives/AccordionExpand/AccordionExpand.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { AccordionExpand } from './AccordionExpand'
+
+const meta: Meta<typeof AccordionExpand> = {
+  title: 'Primitives/AccordionExpand',
+  component: AccordionExpand,
+}
+
+type Story = StoryObj<typeof AccordionExpand>
+
+const items = [
+  {
+    label: 'Option 1',
+    onClick: () => {
+      alert('Clicked option 1')
+    },
+  },
+  { label: 'Option 2', url: '/path' },
+  { label: 'Disabled Option', isDisabled: true },
+]
+
+const Default: Story = {
+  args: {
+    label: 'Accordion Label',
+    items,
+  },
+}
+
+const WithTooltip: Story = {
+  args: {
+    label: 'Accordion With Tooltip',
+    tooltip: 'Tooltip text here',
+    items,
+  },
+}
+
+const Disabled: Story = {
+  args: {
+    label: 'Disabled Accordion',
+    isDisabled: true,
+    items,
+  },
+}
+
+export default meta
+
+export { Default, WithTooltip, Disabled }
