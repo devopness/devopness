b2bd89e77e1ca015432f215b4350a7bd2a9f5f94
WillianSantosC <85417990+WillianSantosC@users.noreply.github.com>
2025-09-24 15:46:08 -0300

    feat(design-system): add Autocomplete component (#2202)

---
+++ packages/ui/react/src/components/Forms/Autocomplete/Autocomplete.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Autocomplete } from './Autocomplete'
+import type { AutocompleteProps } from './Autocomplete'
+
+const meta: Meta<AutocompleteProps> = {
+  title: 'Form/Autocomplete',
+  component: Autocomplete,
+  args: {
+    inputProps: {
+      labelProps: {
+        value: 'Source Provider',
+      },
+      placeholder: 'Select or type to search',
+    },
+    autocompleteProps: {
+      options: [
+        'github',
+        'gitlab',
+        'bitbucket',
+      ],
+    },
+  },
+}
+
+type Story = StoryObj<AutocompleteProps>
+
+const Default: Story = {
+  args: {},
+}
+
+export default meta
+export { Default }
