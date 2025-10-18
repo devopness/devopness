992286afe403eea02d943cb3662dcc93bb19f4f4
Diegiwg <diegiwg@gmail.com>
2025-10-17 22:04:04 -0300

    fix: imports of removed api-service

--- packages/sdks/javascript/src/services/OrganizationService.ts
+++ packages/sdks/javascript/src/services/OrganizationService.ts
@@ -1,8 +1,6 @@
 import { OrganizationsApiService } from "../api/generated/apis/organizations-api";
 import { OrganizationsEnvironmentsApiService } from "../api/generated/apis/organizations-environments-api";
-import { OrganizationsProjectsApiService } from "../api/generated/apis/organizations-projects-api";
 
 export class OrganizationService extends OrganizationsApiService {
   environments = new OrganizationsEnvironmentsApiService();
-  projects = new OrganizationsProjectsApiService();
 }
