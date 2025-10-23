61fe2fd7d5b6e237fcb9ce346d448e0c0ade96b2
Diegiwg <diegiwg@gmail.com>
2025-10-23 11:46:48 -0300

    feat: add TeamsMembershipsApiService to TeamService

--- packages/sdks/javascript/src/services/TeamService.ts
+++ packages/sdks/javascript/src/services/TeamService.ts
@@ -1,8 +1,10 @@
 import { TeamsApiService } from '../api/generated/apis/teams-api';
 import { TeamsInvitationsApiService } from '../api/generated/apis/teams-invitations-api';
 import { TeamsMembersApiService } from '../api/generated/apis/teams-members-api';
+import { TeamsMembershipsApiService } from '../api/generated/apis/teams-memberships-api';
 
 export class TeamService extends TeamsApiService {
     public invitations = new TeamsInvitationsApiService();
     public members = new TeamsMembersApiService();
+    public memberships = new TeamsMembershipsApiService();
 }
