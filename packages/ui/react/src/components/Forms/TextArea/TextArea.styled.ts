86b1190a431899d6b70ee37b1b7870d1da748a0b
Aladino Borges <eualadino@gmail.com>
2025-12-26 15:44:42 -0300

    fix: update TextArea component props to use transient naming convention

--- packages/ui/react/src/components/Forms/TextArea/TextArea.styled.ts
+++ packages/ui/react/src/components/Forms/TextArea/TextArea.styled.ts
@@ -1,4 +1,4 @@
-import { styled, css } from 'styled-components'
+import { css, styled } from 'styled-components'
 
 import { getColor } from 'src/colors'
 import { getFont } from 'src/fonts'
@@ -7,10 +7,10 @@ import { getImageAssetUrl } from 'src/icons'
 const EXPAND_AREA_IMAGE = getImageAssetUrl('expand_area.svg')
 
 type PropsStyled = {
-  hasError?: boolean
-  noResize?: boolean
-  disabled?: boolean
-  readOnly?: boolean
+  $hasError?: boolean
+  $noResize?: boolean
+  $disabled?: boolean
+  $readOnly?: boolean
 }
 
 const removeBlueMark = css`
@@ -34,6 +34,7 @@ const Container = styled.div`
   ${removeBlueMark}
 `
 
+
 const StyledTextarea = styled.textarea<PropsStyled>`
   font-family: ${getFont('mono')};
   width: 100%;
@@ -41,31 +42,31 @@ const StyledTextarea = styled.textarea<PropsStyled>`
 
   font-size: 13px;
   padding: 12px 15px;
-  background: ${({ hasError }) =>
-    getColor(hasError ? 'red.150' : 'indigo.100')};
+  background: ${({ $hasError }) =>
+    getColor($hasError ? 'red.150' : 'indigo.100')};
   border: 1px solid
-    ${({ hasError }) => getColor(hasError ? 'red.500' : 'slate.300')};
+    ${({ $hasError }) => getColor($hasError ? 'red.500' : 'slate.300')};
   border-radius: 10px;
   box-sizing: border-box;
   overflow-y: auto;
-  opacity: ${({ readOnly }) => (readOnly ? '0.5' : '1')};
+  opacity: ${({ $readOnly }) => ($readOnly ? '0.5' : '1')};
   color: ${getColor('blue.950')};
-  resize: ${({ noResize }) => (noResize ? 'none' : 'vertical')};
+  resize: ${({ $noResize }) => ($noResize ? 'none' : 'vertical')};
 
   &:hover,
   &:focus {
-    border-color: ${({ hasError }) =>
-      hasError ? getColor('red.500') : getColor('purple.800')};
-    ${({ readOnly }) =>
-      readOnly && `border: 1px solid ${getColor('slate.300')}`}
+    border-color: ${({ $hasError }) =>
+    $hasError ? getColor('red.500') : getColor('purple.800')};
+    ${({ $readOnly }) =>
+    $readOnly && `border: 1px solid ${getColor('slate.300')}`}
   }
 
   &::placeholder {
     font-family: ${getFont('roboto')};
-    color: ${({ hasError, readOnly }) =>
-      readOnly
+    color: ${({ $hasError, $readOnly }) =>
+    $readOnly
         ? '#6F737C'
-        : hasError
+    : $hasError
           ? getColor('gray.615')
           : getColor('slate.400')};
     font-size: 13px;
