0a9b3a850524fb7c8e9113791a67fb4ae440392c
Aladino Borges <eualadino@gmail.com>
2025-12-27 13:16:32 -0300

    fix: update FlexContainer component props to use transient properties

--- packages/ui/react/src/components/Primitives/FlexContainer/FlexContainer.tsx
+++ packages/ui/react/src/components/Primitives/FlexContainer/FlexContainer.tsx
@@ -33,9 +33,17 @@ type FlexContainerProps = {
  * </FlexContainer>
  * ```
  */
-const FlexContainer = ({ children, ...props }: FlexContainerProps) => (
-  <FlexContainerWrapper {...props}>{children}</FlexContainerWrapper>
+const FlexContainer = ({ children, direction, align, wrap, gap, justify }: FlexContainerProps) => (
+  <FlexContainerWrapper
+    $direction={direction}
+    $align={align}
+    $wrap={wrap}
+    $gap={gap}
+    $justify={justify}
+  >
+    {children}
+  </FlexContainerWrapper>
 )
 
-export type { FlexContainerProps }
 export { FlexContainer }
+export type { FlexContainerProps }
