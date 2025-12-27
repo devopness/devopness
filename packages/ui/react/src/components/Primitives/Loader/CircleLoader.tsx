31be683e0c8e5863274760dc92485dfeaa0da312
Aladino Borges <eualadino@gmail.com>
2025-12-27 10:52:46 -0300

    fix: update CircleLoader component props to use transient properties

--- packages/ui/react/src/components/Primitives/Loader/CircleLoader.tsx
+++ packages/ui/react/src/components/Primitives/Loader/CircleLoader.tsx
@@ -1,15 +1,15 @@
 import { CircleLoader as CometSpinLoader } from 'react-spinners'
 
+import { getColor } from 'src/colors'
 import { ContainerCometSpinLoader } from './Loader.styled'
 import type { ReactSpinnersProps } from './ReactSpinners.type'
-import { getColor } from 'src/colors'
 
 type CircleLoaderProps = {
   paddingTop?: string
 } & ReactSpinnersProps
 
 const CircleLoader = ({ paddingTop, ...props }: CircleLoaderProps) => (
-  <ContainerCometSpinLoader paddingTop={paddingTop}>
+  <ContainerCometSpinLoader $paddingTop={paddingTop}>
     <CometSpinLoader
       color={getColor('purple.800')}
       size={20}
