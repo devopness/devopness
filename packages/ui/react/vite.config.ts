bdeb1ca1da5fccaf6fe2b08a2cc993fbc3563f16
Diegiwg <diegiwg@gmail.com>
2026-01-22 18:54:08 -0300

    chore: typo

--- packages/ui/react/vite.config.ts
+++ packages/ui/react/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
