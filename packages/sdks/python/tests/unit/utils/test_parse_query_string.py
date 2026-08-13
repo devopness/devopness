b2d04a3eeb1e73b18f17bd4564312f0903680153
Diegiwg <diegiwg@gmail.com>
2026-08-13 17:20:19 +0000

    fix(sdk): support filter query params

--- packages/sdks/python/tests/unit/utils/test_parse_query_string.py
+++ packages/sdks/python/tests/unit/utils/test_parse_query_string.py
@@ -4,26 +4,71 @@ from devopness.generated.utils import parse_query_string
 
 
 class TestParseQueryString(unittest.TestCase):
-    def test_parse_query_string(self) -> None:
+    def test_parse_query_string_keeps_scalar_values_and_omits_empty_ones(self) -> None:
         params = {
             "string": "hello",
             "integer": 123,
             "float": 123.456,
             "boolean": True,
-            "list": ["a", "b", "c"],
-            "dict": {"a": 1, "b": 2},
-            "null": None,
+            "false_value": False,
+            "null_value": None,
             "empty_string": "",
             "empty_list": [],
             "empty_dict": {},
         }
 
         query_string = parse_query_string(params)
+
         expected_query_string = "string=hello"
         expected_query_string += "&integer=123"
         expected_query_string += "&float=123.456"
         expected_query_string += "&boolean=True"
-        expected_query_string += "&list=%5B%27a%27%2C+%27b%27%2C+%27c%27%5D"
-        expected_query_string += "&dict=%7B%27a%27%3A+1%2C+%27b%27%3A+2%7D"
+
+        self.assertEqual(query_string, expected_query_string)
+
+    def test_parse_query_string_preserves_zero_values(self) -> None:
+        params = {
+            "page": 0,
+            "limit": 0,
+        }
+
+        query_string = parse_query_string(params)
+
+        expected_query_string = "page=0"
+        expected_query_string += "&limit=0"
+
+        self.assertEqual(query_string, expected_query_string)
+
+    def test_parse_query_string_expands_filter_parameters(self) -> None:
+        params = {
+            "page": 1,
+            "filter": {
+                "organization_id": 123,
+                "project_id": 0,
+                "status": "queued",
+                "empty_value": "",
+                "empty_list": [],
+                "empty_dict": {},
+            },
+        }
+
+        query_string = parse_query_string(params)
+
+        expected_query_string = "page=1"
+        expected_query_string += "&filter%5Borganization_id%5D=123"
+        expected_query_string += "&filter%5Bproject_id%5D=0"
+        expected_query_string += "&filter%5Bstatus%5D=queued"
+
+        self.assertEqual(query_string, expected_query_string)
+
+    def test_parse_query_string_ignores_invalid_filter_values(self) -> None:
+        params = {
+            "filter": None,
+            "other": "value",
+        }
+
+        query_string = parse_query_string(params)
+
+        expected_query_string = "other=value"
 
         self.assertEqual(query_string, expected_query_string)
