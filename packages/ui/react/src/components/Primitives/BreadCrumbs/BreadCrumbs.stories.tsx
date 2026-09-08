1dd6a7a8e627c1f0fcff7ac7417e07fffc64a375
therealrinku <rinkunited2032@gmail.com>
2026-09-08 15:17:43 +0545

    feat: add stories with icon

--- packages/ui/react/src/components/Primitives/BreadCrumbs/BreadCrumbs.stories.tsx
+++ packages/ui/react/src/components/Primitives/BreadCrumbs/BreadCrumbs.stories.tsx
@@ -43,6 +43,62 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
 
+export const WithIcons: Story = {
+  args: {
+    navigateCrumbs: [
+      {
+        label: 'Devopness',
+        list: [],
+        onClick: () => {},
+        badge: {
+          icon: true,
+          name: 'organization',
+          backgroundColor: '#786efd',
+        },
+      },
+      {
+        label: 'Production',
+        list: [
+          {
+            label: 'Staging',
+            onClick: () => {},
+            badge: {
+              icon: true,
+              name: 'server',
+              backgroundColor: '#4285f4',
+            },
+          },
+          {
+            label: 'Development',
+            onClick: () => {},
+            badge: {
+              icon: true,
+              name: 'devices',
+              backgroundColor: '#9c6ade',
+            },
+          },
+        ],
+        onClick: () => {},
+        badge: {
+          icon: true,
+          name: 'server',
+          backgroundColor: '#4285f4',
+        },
+      },
+      {
+        label: 'Application',
+        list: [],
+        onClick: () => {},
+        badge: {
+          icon: true,
+          name: 'devices',
+          backgroundColor: '#9c6ade',
+        },
+      },
+    ],
+  },
+}
+
 export const Loading: Story = {
   args: {
     isLoading: true,
