41e359cf0e8d44837dec08ffab295cd7ffa11b6a
Diegiwg <diegiwg@gmail.com>
2026-09-11 12:16:30 -0300

    chore: format

--- docs/src/app/[[...slug]]/layout.tsx
+++ docs/src/app/[[...slug]]/layout.tsx
@@ -6,8 +6,8 @@ import { source } from "@/lib/source";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-      // TODO: Restore the top bar. When `tabs` are enabled on `DocsLayout`, the top bar is replaced
-      // and its links are merged into the navbar/sidebar pattern, which is not what we want.
+    // TODO: Restore the top bar. When `tabs` are enabled on `DocsLayout`, the top bar is replaced
+    // and its links are merged into the navbar/sidebar pattern, which is not what we want.
     <DocsLayout tabs={layoutTabs} tree={source.pageTree} {...baseOptions()}>
       {children}
     </DocsLayout>
